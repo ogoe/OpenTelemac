5e778d52d6d3769bb6aba971e3e0065ff5a81afb
s.bourban <s.bourban@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2011-03-07 02:22:36 +0000

    Application of standard template for clarity of Doxygen interpretation
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@115 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 9ee97d99243878d7c9aa1645fd8d38c4e94657da

--- mumpsvoid/mumpsvoid_v6p1/sources/pre4_mumps.f
+++ mumpsvoid/mumpsvoid_v6p1/sources/pre4_mumps.f
@@ -1,151 +1,63 @@
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @brief       CALLS THE DIRECT SOLVER MUMPS.
-!>                IF MUMPS IS NOT INSTALLED : EMPTY SUBROUTINES ARE USED INSTEAD.
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Variable(s)
-!>  <br><table>
-!>     <tr><th> Argument(s)
-!>    </th><td> CVB1, CVB2, DAB1, DAB2, DAB3, DAB4, GLOSEGB, INFOGR, NPOIN, NSEGB, TYPEXT, XAB1, XAB2, XAB3, XAB4, XX1, XX2
-!>   </td></tr>
-!>     <tr><th> Common(s)
-!>    </th><td>
-!> INFO : LNG, LU
-!>   </td></tr>
-!>     </table>
-
-!>  @par Call(s)
-!>  <br><table>
-!>     <tr><th> Known(s)
-!>    </th><td> PLANTE()
-!>   </td></tr>
-!>     </table>
-
-!>  @par Called by
-!><br>SOLVE()
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Development history
-!>   <br><table>
-!> <tr><th> Release </th><th> Date </th><th> Author </th><th> Notes </th></tr>
-!>  <tr><td><center> 6.0                                       </center>
-!>    </td><td> 21/08/2010
-!>    </td><td> N.DURAND (HRW), S.E.BOURBAN (HRW)
-!>    </td><td> Creation of DOXYGEN tags for automated documentation and cross-referencing of the FORTRAN sources
-!>   </td></tr>
-!>  <tr><td><center> 6.0                                       </center>
-!>    </td><td> 13/07/2010
-!>    </td><td> N.DURAND (HRW), S.E.BOURBAN (HRW)
-!>    </td><td> Translation of French comments within the FORTRAN sources into English comments
-!>   </td></tr>
-!>  <tr>
-!>    <td><center> 5.9                                    </center></td>
-!>    <td> 14/10/2009                                              </td>
-!>    <td> C. DENIS (SINETICS)                                     </td>
-!>    <td>                                                         </td>
-!>  <tr>
-!>    <td><center>                                        </center></td>
-!>    <td> 20/11/2006                                              </td>
-!>    <td> E. RAZAFINDRAKOTO (LNH) 01 30 87 74 03                  </td>
-!>    <td>                                                         </td>
-!>  </table>
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Details of primary variable(s)
-!>  <br><table>
-!>
-!>     <tr><th>Name(s)</th><th>(in-out)</th><th>Description</th></tr>
-!>          <tr><td>CVB1,CVB2
-!></td><td>--></td><td>SECONDS MEMBRES
-!>    </td></tr>
-!>          <tr><td>DA,XA
-!></td><td>--></td><td>DIAGONALES ET TERMES EXTRA-DIAGONAUX DES
-!>                  MATRICES
-!>    </td></tr>
-!>          <tr><td>DAB1
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>DAB2
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>DAB3
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>DAB4
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>GLOSEG
-!></td><td>--></td><td>NUMEROS GLOBAUX DES POINTS DES SEGMENTS
-!>    </td></tr>
-!>          <tr><td>GLOSEGB
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>INFOGR
-!></td><td>--></td><td>IF, YES INFORMATIONS ON LISTING
-!>    </td></tr>
-!>          <tr><td>NPOIN
-!></td><td>--></td><td>NOMBRE D'INCONNUES
-!>    </td></tr>
-!>          <tr><td>NSEGB
-!></td><td>--></td><td>NOMBRE DE SEGMENTS
-!>    </td></tr>
-!>          <tr><td>TYPEXT
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XAB1
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XAB2
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XAB3
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XAB4
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XX1,XX2
-!></td><td><--</td><td>SOLUTIONS
-!>    </td></tr>
-!>     </table>
-C
-C#######################################################################
-C
-                       SUBROUTINE PRE4_MUMPS
+!                    *********************
+                     SUBROUTINE PRE4_MUMPS
+!                    *********************
+!
      &(NPOIN,NSEGB,GLOSEGB,DAB1,DAB2,DAB3,DAB4,XAB1,XAB2,XAB3,XAB4,
      & XX1,XX2,CVB1,CVB2,INFOGR,TYPEXT)
-C
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-C| CVB1,CVB2      |-->| SECONDS MEMBRES
-C| DA,XA          |-->| DIAGONALES ET TERMES EXTRA-DIAGONAUX DES
-C|                |   | MATRICES
-C| DAB1           |---| 
-C| DAB2           |---| 
-C| DAB3           |---| 
-C| DAB4           |---| 
-C| GLOSEG         |-->| NUMEROS GLOBAUX DES POINTS DES SEGMENTS
-C| GLOSEGB        |---| 
-C| INFOGR         |-->| IF, YES INFORMATIONS ON LISTING
-C| NPOIN          |-->| NOMBRE D'INCONNUES
-C| NSEGB          |-->| NOMBRE DE SEGMENTS
-C| TYPEXT         |---| 
-C| XAB1           |---| 
-C| XAB2           |---| 
-C| XAB3           |---| 
-C| XAB4           |---| 
-C| XX1,XX2        |<--| SOLUTIONS
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-C
+!
+!***********************************************************************
+! MUMPSVOID   V6P0                                   21/08/2010
+!***********************************************************************
+!
+!brief    CALLS THE DIRECT SOLVER MUMPS.
+!+                IF MUMPS IS NOT INSTALLED : EMPTY SUBROUTINES ARE USED INSTEAD.
+!
+!history  E. RAZAFINDRAKOTO (LNH)
+!+        20/11/2006
+!+        
+!+   
+!
+!history  C. DENIS (SINETICS)
+!+        14/10/2009
+!+        V5P9
+!+   
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into 
+!+   English comments 
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and 
+!+   cross-referencing of the FORTRAN sources 
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!| CVB1,CVB2      |-->| SECONDS MEMBRES
+!| DAB1           |---| 
+!| DAB2           |---| 
+!| DAB3           |---| 
+!| DAB4           |---| 
+!| GLOSEGB        |---| 
+!| INFOGR         |-->| IF, YES INFORMATIONS ON LISTING
+!| NPOIN          |-->| NOMBRE D'INCONNUES
+!| NSEGB          |-->| NOMBRE DE SEGMENTS
+!| TYPEXT         |---| 
+!| XAB1           |---| 
+!| XAB2           |---| 
+!| XAB3           |---| 
+!| XAB4           |---| 
+!| XX1,XX2        |<--| SOLUTIONS
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
       IMPLICIT NONE
       INTEGER LNG,LU
       COMMON/INFO/LNG,LU
-C
-C+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-C
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
       INTEGER, INTENT(IN) :: NPOIN,NSEGB
       INTEGER, INTENT(IN) :: GLOSEGB(NSEGB*2)
       LOGICAL, INTENT(IN) :: INFOGR
@@ -156,8 +68,7 @@ C
       DOUBLE PRECISION, INTENT(INOUT) :: XX1(NPOIN),XX2(NPOIN)
       DOUBLE PRECISION, INTENT(IN)    :: CVB1(NPOIN),CVB2(NPOIN)
       CHARACTER(LEN=1), INTENT(IN)    :: TYPEXT
-C
-
+!
       IF(LNG.EQ.1) WRITE(LU,2018)
       IF(LNG.EQ.2) WRITE(LU,2019)
 2018  FORMAT(1X,'MUMPS NON INSTALLE SUR CE SYSTEME,',/,1X,
@@ -167,15 +78,10 @@ C
       CALL PLANTE(1)
       STOP
 
-
-C
-C-----------------------------------------------------------------------
-C
-
-C-----------------------------------------------------------------------
-C
+!
+!-----------------------------------------------------------------------
+!
+!-----------------------------------------------------------------------
+!
       RETURN
       END
-C
-C#######################################################################
-C
--- mumpsvoid/mumpsvoid_v6p1/sources/solve_mumps.f
+++ mumpsvoid/mumpsvoid_v6p1/sources/solve_mumps.f
@@ -1,115 +1,55 @@
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @brief       CALLS THE DIRECT SOLVER MUMPS
-!>                IF MUMPS IS NOT INSTALLED : EMPTY SUBROUTINES ARE USED INSTEAD.
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Variable(s)
-!>  <br><table>
-!>     <tr><th> Argument(s)
-!>    </th><td> DA, GLOSEG, INFOGR, LT, MAXSEG, NPOIN, NSEGB, RHS, TYPEXT, XA, XINC
-!>   </td></tr>
-!>     <tr><th> Common(s)
-!>    </th><td>
-!> INFO : LNG, LU
-!>   </td></tr>
-!>     </table>
-
-!>  @par Call(s)
-!>  <br><table>
-!>     <tr><th> Known(s)
-!>    </th><td> PLANTE()
-!>   </td></tr>
-!>     </table>
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Development history
-!>   <br><table>
-!> <tr><th> Release </th><th> Date </th><th> Author </th><th> Notes </th></tr>
-!>  <tr><td><center> 6.0                                       </center>
-!>    </td><td> 21/08/2010
-!>    </td><td> N.DURAND (HRW), S.E.BOURBAN (HRW)
-!>    </td><td> Creation of DOXYGEN tags for automated documentation and cross-referencing of the FORTRAN sources
-!>   </td></tr>
-!>  <tr><td><center> 6.0                                       </center>
-!>    </td><td> 13/07/2010
-!>    </td><td> N.DURAND (HRW), S.E.BOURBAN (HRW)
-!>    </td><td> Translation of French comments within the FORTRAN sources into English comments
-!>   </td></tr>
-!>  <tr>
-!>    <td><center> 5.7                                    </center></td>
-!>    <td> 02/11/2009                                              </td>
-!>    <td> C. DENIS (SINETICS)                                     </td>
-!>    <td>                                                         </td>
-!>  <tr>
-!>    <td><center>                                        </center></td>
-!>    <td> 14/10/2009                                              </td>
-!>    <td> F. ZAOUI / C. DENIS (LNHE/SINETICS)                     </td>
-!>    <td>                                                         </td>
-!>  </table>
-
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-!>  @par Details of primary variable(s)
-!>  <br><table>
-!>
-!>     <tr><th>Name(s)</th><th>(in-out)</th><th>Description</th></tr>
-!>          <tr><td>DA,XA
-!></td><td>--></td><td>DIAGONALE ET TERMES EXTRA-DIAGONAUX DE LA MATRICE
-!>    </td></tr>
-!>          <tr><td>GLOSEG
-!></td><td>--></td><td>NUMEROS GLOBAUX DES POINTS DES SEGMENTS
-!>    </td></tr>
-!>          <tr><td>INFOGR
-!></td><td>--></td><td>IF, YES INFORMATIONS ON LISTING
-!>    </td></tr>
-!>          <tr><td>LT
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>MAXSEG
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>NPOIN
-!></td><td>--></td><td>NOMBRE D'INCONNUES
-!>    </td></tr>
-!>          <tr><td>NSEGB
-!></td><td>--></td><td>NOMBRE DE SEGMENTS
-!>    </td></tr>
-!>          <tr><td>RHS
-!></td><td>--></td><td>SECOND MEMBRE
-!>    </td></tr>
-!>          <tr><td>TYPEXT
-!></td><td>---</td><td>
-!>    </td></tr>
-!>          <tr><td>XINC
-!></td><td><--</td><td>SOLUTION
-!>    </td></tr>
-!>     </table>
-C
-C#######################################################################
-C
-                      SUBROUTINE SOLVE_MUMPS
+!                    **********************
+                     SUBROUTINE SOLVE_MUMPS
+!                    **********************
+!
      &(NPOIN,NSEGB,GLOSEG,MAXSEG,DA,XA,XINC,RHS,INFOGR,TYPEXT,LT)
-C
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-C| DA,XA          |-->| DIAGONALE ET TERMES EXTRA-DIAGONAUX DE LA MATRICE
-C| GLOSEG         |-->| NUMEROS GLOBAUX DES POINTS DES SEGMENTS
-C| INFOGR         |-->| IF, YES INFORMATIONS ON LISTING
-C| LT             |---| 
-C| MAXSEG         |---| 
-C| NPOIN          |-->| NOMBRE D'INCONNUES
-C| NSEGB          |-->| NOMBRE DE SEGMENTS
-C| RHS            |-->| SECOND MEMBRE
-C| TYPEXT         |---| 
-C| XINC           |<--| SOLUTION
-C~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-C
+!
+!***********************************************************************
+! MUMPSVOID   V6P0                                   21/08/2010
+!***********************************************************************
+!
+!brief    CALLS THE DIRECT SOLVER MUMPS
+!+                IF MUMPS IS NOT INSTALLED : EMPTY SUBROUTINES ARE USED INSTEAD.
+!
+!history  F. ZAOUI / C. DENIS (LNHE/SINETICS)
+!+        14/10/2009
+!+        
+!+   
+!
+!history  C. DENIS (SINETICS)
+!+        02/11/2009
+!+        V5P7
+!+   
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into 
+!+   English comments 
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and 
+!+   cross-referencing of the FORTRAN sources 
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!| DA,XA          |-->| DIAGONALE ET TERMES EXTRA-DIAGONAUX DE LA MATRICE
+!| GLOSEG         |-->| NUMEROS GLOBAUX DES POINTS DES SEGMENTS
+!| INFOGR         |-->| IF, YES INFORMATIONS ON LISTING
+!| LT             |---| 
+!| MAXSEG         |---| 
+!| NPOIN          |-->| NOMBRE D'INCONNUES
+!| NSEGB          |-->| NOMBRE DE SEGMENTS
+!| RHS            |-->| SECOND MEMBRE
+!| TYPEXT         |---| 
+!| XINC           |<--| SOLUTION
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
       IMPLICIT NONE
-C
-C+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-C ARGUMENTS
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+! ARGUMENTS
       INTEGER, INTENT(IN)             :: NPOIN,NSEGB,MAXSEG
       INTEGER, INTENT(IN)             :: GLOSEG(MAXSEG,2)
       INTEGER, INTENT(IN)             :: LT
@@ -120,7 +60,6 @@ C ARGUMENTS
       COMMON/INFO/LNG,LU
       INTEGER LNG,LU
 
-
       IF(LNG.EQ.1) WRITE(LU,2018)
       IF(LNG.EQ.2) WRITE(LU,2019)
 2018  FORMAT(1X,'MUMPS NON INSTALLE SUR CE SYSTEME,',/,1X,
@@ -130,8 +69,4 @@ C ARGUMENTS
       CALL PLANTE(1)
       STOP
 
-
       END
-C
-C#######################################################################
-C
