6fb7cd9f7ffb608149d1a8ac8f4d19b80bec5b24
s.bourban <s.bourban@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2011-07-25 11:19:06 +0000

    Addition of the g95 configuration -- this file is to be edited by the user in case of the Perl version (which is not the case for the Python version).
    /!\ make from g95 does not seem to work -- use of nmake instead.
    
    
    
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@576 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: e33d168ab37b7f12b424c43da3aa324bdd8c78dc

--- bin/maktel.bat
+++ bin/maktel.bat
@@ -7,10 +7,14 @@ rem ---------------------------DeltaCAD-Septembre98---
 rem
 rem
 rem Please activate the following line if you are using INTEL or COMPAQ compiler
-rem  
-     nmake /f makefile.wnt %1 %2 %3
 rem
-rem Please activate the following line if you are using GFORTRAN or G95 compiler
+  nmake /f makefile.wnt %1 %2 %3
+rem
+rem Please activate the following line if you are using G95 compiler
+rem
+rem  nmake /f makefile.gfo %1 %2 %3
+rem
+rem Please activate the following line if you are using GFORTRAN compiler
 rem
 rem gfomake /f makefile.gfo %1 %2 %3
 rem
