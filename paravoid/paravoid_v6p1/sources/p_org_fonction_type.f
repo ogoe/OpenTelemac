09a94b75b94525a11d6d73639f9d05c8714002d9
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2011-07-29 14:05:20 +0000

    passage a MPI2, 4 routines en provenance de Tomawac
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@611 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: e72d967dc95294e6f3b5904f2125fe3fcdf52fb6

---
+++ paravoid/paravoid_v6p1/sources/p_org_fonction_type.f
@@ -0,0 +1,53 @@
+!                    ******************************
+                     SUBROUTINE P_ORG_FONCTION_TYPE 
+!                    ******************************
+! 
+     &(NOMB,TRACE,CHARACTERISTIC)                      
+!
+!***********************************************************************
+! PARAVOID   V6P1                                   21/08/2010
+!***********************************************************************
+!
+!brief    MPI TYPE FOR TYPE CHARAC_TYPE - CHARACTERISTICS /
+!+        USED BY TOMAWAC ONLY
+!
+!history  C. DENIS
+!+        01/07/2011
+!+        V6P1
+!+
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into
+!+   English comments
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and
+!+   cross-referencing of the FORTRAN sources
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!| NOMB           |<---| NUMBER OF VARIABLES 
+!| TRACE          |<---| IF .TRUE. TRACE EXECUTION
+!| CHARACTERISTIC |--->| DATATYPE FOR CHARACTERISTIC 
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!         
+      IMPLICIT NONE 
+      INTEGER LNG,LU
+      COMMON/INFO/LNG,LU 
+!       
+      INTEGER, INTENT(INOUT) :: NOMB 
+      INTEGER, INTENT(INOUT)  :: CHARACTERISTIC 
+      LOGICAL, INTENT(IN) ::TRACE 
+!
+      IF(LNG.EQ.1) WRITE(LU,*) 'APPEL DE P_ORG_CHARAC_TYPE VERSION VIDE'
+      IF(LNG.EQ.2) WRITE(LU,*) 'CALL OF P_ORG_CHARAC_TYPE VOID VERSION'       
+!     
+!-----------------------------------------------------------------------
+!     
+      RETURN  
+      END 
+ 
+ 
---
+++ paravoid/paravoid_v6p1/sources/p_org_fonction_type_4d.f
@@ -0,0 +1,54 @@
+!                    *********************************
+                     SUBROUTINE P_ORG_FONCTION_TYPE_4D 
+!                    ********************************* 
+! 
+     &(NOMB,TRACE,CHARACTERISTIC)                      
+!
+!***********************************************************************
+! PARAVOID   V6P1                                   21/08/2010
+!***********************************************************************
+!
+!brief    MPI TYPE FOR TYPE CHARAC_TYPE - CHARACTERISTICS /
+!+        USED BY TOMAWAC ONLY
+!
+!history  C. DENIS
+!+        01/07/2011
+!+        V6P1
+!+
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into
+!+   English comments
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and
+!+   cross-referencing of the FORTRAN sources
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!| NOMB           |<---| NUMBER OF VARIABLES 
+!| TRACE          |<---| IF .TRUE. TRACE EXECUTION
+!| CHARACTERISTIC |--->| DATATYPE FOR CHARACTERISTIC 
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!         
+      IMPLICIT NONE 
+!      
+      INTEGER LNG,LU
+      COMMON/INFO/LNG,LU 
+!       
+      INTEGER, INTENT(INOUT) :: NOMB 
+      INTEGER, INTENT(INOUT)  :: CHARACTERISTIC 
+      LOGICAL, INTENT(IN) ::TRACE 
+!
+      IF(LNG.EQ.1) WRITE(LU,*) 'P_ORG_FONCTION_TYPE_4D  VERSION VIDE'
+      IF(LNG.EQ.2) WRITE(LU,*) 'P_ORG_FONCTION_TYPE_4D VOID VERSION'      
+!     
+!----------------------------------------------------------------------
+!     
+      RETURN  
+      END 
+ 
+ 
