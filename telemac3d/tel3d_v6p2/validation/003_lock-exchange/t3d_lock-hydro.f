7ade1496c462184eaa163593232c0f1101eee18e
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2012-05-24 15:40:30 +0000

    cas-test lock-exchange mis a jour
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@1411 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 8d9247fb69e0db777a0b824afc52374fa00de4ea

--- telemac3d/tel3d_v6p2/validation/003_lock-exchange/t3d_lock-hydro.f
+++ telemac3d/tel3d_v6p2/validation/003_lock-exchange/t3d_lock-hydro.f
@@ -1,21 +1,56 @@
-!                       *****************
-                        SUBROUTINE CONDIM 
-!                       *****************
-! 
+!                    *****************
+                     SUBROUTINE CONDIM
+!                    *****************
+!
 !
 !***********************************************************************
-! TELEMAC-3D   V5.1       
-! fortran95 version         march 1999        Jacek A. Jankowski pinxit
+! TELEMAC3D   V6P1                                   21/08/2010
 !***********************************************************************
 !
-!      FONCTION:
-!      =========
+!brief    INITIALISES VELOCITY, DEPTH AND TRACERS.
 !
-!    INITIALISATION DES TABLEAUX DES GRANDEURS PHYSIQUES
+!history  JACEK A. JANKOWSKI PINXIT
+!+        **/03/1999
+!+
+!+   FORTRAN95 VERSION
 !
-!***********************************************************************
+!history
+!+        20/04/2007
+!+
+!+   ADDED INITIALISATION OF DPWAVE
+!
+!history
+!+        23/01/2009
+!+
+!+   ADDED CHECK OF ZSTAR
+!
+!history
+!+        16/03/2010
+!+
+!+   NEW OPTIONS FOR BUILDING THE MESH IN CONDIM, SEE BELOW
+!
+!history  J-M HERVOUET(LNHE)
+!+        05/05/2010
+!+        V6P0
+!+   SUPPRESSED INITIALISATION OF DPWAVE
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into
+!+   English comments
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and
+!+   cross-referencing of the FORTRAN sources
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !
       USE BIEF
+      USE INTERFACE_TELEMAC3D, EX_CONDIM => CONDIM
       USE DECLARATIONS_TELEMAC
       USE DECLARATIONS_TELEMAC3D
 !
@@ -25,73 +60,201 @@
 !
 !-----------------------------------------------------------------------
 !
-      INTEGER IPLAN, I, ipoin2, ipoin3 
+      INTEGER IPLAN, I,J,IPOIN3,IPOIN2
 !
 !***********************************************************************
-! TIME ORIGIN
 !
-      AT  = 0.D0
+!     ORIGIN OF TIME
 !
-! INITIALISATION DE H , LA HAUTEUR D'EAU.
+      IF(.NOT.SUIT2) AT  = 0.D0
 !
-      CALL OS( 'X=C     ' , H   , H , H , 0.D0)
-      CALL OV( 'X=X-Y   ' , H%R , Z , Z , 0.D0 , NPOIN2 )
+!     INITIALISES H, THE WATER DEPTH
+!
+      IF(.NOT.SUIT2) THEN
+!
+      IF(CDTINI(1:10).EQ.'COTE NULLE'.OR.
+     &   CDTINI(1:14).EQ.'ZERO ELEVATION') THEN
+        CALL OS( 'X=C     ' ,X=H,C=0.D0)
+        CALL OV( 'X=X-Y   ' , H%R , Z , Z , 0.D0 , NPOIN2 )
+      ELSEIF(CDTINI(1:14).EQ.'COTE CONSTANTE'.OR.
+     &       CDTINI(1:18).EQ.'CONSTANT ELEVATION') THEN
+        CALL OS( 'X=C     ' ,X=H,C=COTINI)
+        CALL OV( 'X=X-Y   ' , H%R , Z , Z , 0.D0 , NPOIN2 )
+      ELSEIF(CDTINI(1:13).EQ.'HAUTEUR NULLE'.OR.
+     &       CDTINI(1:10).EQ.'ZERO DEPTH') THEN
+        CALL OS( 'X=C     ' ,X=H,C=0.D0)
+      ELSEIF(CDTINI(1:17).EQ.'HAUTEUR CONSTANTE'.OR.
+     &       CDTINI(1:14).EQ.'CONSTANT DEPTH') THEN
+        CALL OS( 'X=C     ' ,X=H,C=HAUTIN)
+      ELSEIF(CDTINI(1:13).EQ.'PARTICULIERES'.OR.
+     &       CDTINI(1:10).EQ.'PARTICULAR'.OR.
+     &       CDTINI(1:07).EQ.'SPECIAL') THEN
+!     USER INPUT :
+!     PROGRAM HERE SPECIAL INITIAL CONDITIONS ON DEPTH
+        IF(LNG.EQ.1) WRITE(LU,10)
+        IF(LNG.EQ.2) WRITE(LU,11)
+10      FORMAT(1X,'CONDIM : AVEC DES CONDITIONS INITIALES PARTICULIERES'
+     &      ,/,1X,'         VOUS DEVEZ MODIFIER CONDIM')
+11      FORMAT(1X,'CONDIM : WITH SPECIAL INITIAL CONDITIONS'
+     &      ,/,1X,'         YOU HAVE TO MODIFY CONDIM')
+        CALL PLANTE(1)
+        STOP
+!     END OF SPECIAL INITIAL CONDITIONS
+!     END OF USER INPUT
+      ELSE
+        IF(LNG.EQ.1) THEN
+        WRITE(LU,*) 'CONDIM : CONDITION INITIALE NON PREVUE : ',CDTINI
+        ENDIF
+        IF(LNG.EQ.2) THEN
+        WRITE(LU,*) 'CONDIM: INITIAL CONDITION UNKNOWN: ',CDTINI
+        ENDIF
+        STOP
+      ENDIF
+      ELSE
+        IF(LNG.EQ.1) WRITE(LU,*) 'HAUTEUR LUE DANS LE FICHIER BINAIRE 1'
+        IF(LNG.EQ.2) WRITE(LU,*) 'DEPTH IS READ IN THE BINARY FILE 1'
+      ENDIF
+!
+!     CLIPS H
 !
       DO I=1,NPOIN2
-       H%R(I)=MAX(H%R(I),HMIN)
+        H%R(I)=MAX(H%R(I),0.D0)
       ENDDO
 !
-      call OS ('X=Y     ', hn, h, h, 0.d0)
+      CALL OS ('X=Y     ',X=HN,Y=H)
 !
 !-----------------------------------------------------------------------
 !
-      IF (NPLINT.GE.2) THEN
-        CALL OV( 'X=C     ' ,Z((NPLINT-1)*NPOIN2+1:NPLINT*NPOIN2)
-     *                      ,Z,Z,COTINT,NPOIN2)
-      ENDIF
+!     DATA TO BUILD VERTICAL COORDINATES IN CALCOT
+!
+!     TRANSF IS KEYWORD "MESH TRANSFORMATION"
+!     IF TRANSF = 0, SUBROUTINE CALCOT MUST BE IMPLEMENTED BY THE USER
+!
+!     AN EQUIVALENT OF TRANSF MUST BE GIVEN FOR EVERY PLANE:
+!
+!     POSSIBLE VALUES OF TRANSF_PLANE :
+!
+!     1 : SIGMA TRANSFORMATION WITH EVENLY SPACED PLANES
+!     2 : SIGMA TRANSFORMATION WITH PROPORTIONS GIVEN IN ZSTAR
+!     3 : PRESCRIBED ELEVATION GIVEN IN ZPLANE
+!
+!     STANDARD BELOW IS: EVENLY SPACED PLANES, NO OTHER DATA REQUIRED
+!
+      DO IPLAN = 1,NPLAN
+        TRANSF_PLANE%I(IPLAN)=1
+      ENDDO
+!
+!     OTHER EXAMPLES:
+!
+!     EXAMPLE 1: ALL PLANES WITH PRESCRIBED ELEVATION
+!
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=3
+!     ENDDO
+!     ZPLANE%R(2)=-7.D0
+!     ZPLANE%R(3)=-4.D0
+!     ...
+!     ZPLANE%R(NPLAN-1)=-0.05D0
 !
 !
-      IF (NPLINT.GE.2) THEN
-        DO IPLAN = 1,NPLINT-1
-          ZSTAR%R(IPLAN) = DBLE(IPLAN-NPLINT)/DBLE(NPLINT-1)
-        END DO
-      ENDIF
+!     EXAMPLE 2: SIGMA TRANSFORMATION WITH GIVEN PROPORTIONS
 !
-      DO IPLAN = NPLINT,NPLAN
-        ZSTAR%R(IPLAN) = DBLE(IPLAN-NPLINT)/DBLE(NPLAN-NPLINT)
-      END DO
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=2
+!     ENDDO
+!     ZSTAR%R(2)=0.02D0
+!     ZSTAR%R(3)=0.1D0
+!     ...
+!     ZSTAR%R(NPLAN-1)=0.95D0
+!
+!
+!     EXAMPLE 3: ONE PLANE (NUMBER 4) WITH PRESCRIBED ELEVATION
+!                AND SIGMA ELSEWHERE
+!
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=1
+!     ENDDO
+!     TRANSF_PLANE%I(4)=3
+!     ZPLANE%R(4)=-3.D0
+!
+!
+!     EXAMPLE 4: ONE PLANE WITH PRESCRIBED ELEVATION
+!                AND 2 SIGMA TRANSFORMATIONS, WITH NPLAN=7
+!                SIGMA TRANSFORMATIONS ARE MEANT BETWEEN
+!                BOTTOM, FIXED ELEVATION PLANES AND FREE SURFACE
+!                THE VALUES OF ZSTAR ARE LOCAL FOR EVERY
+!                SIGMA TRANSFORMATION: 0. FOR LOWER FIXED PLANE
+!                                      1. FOR UPPER FIXED PLANE
+!
+!     DO IPLAN = 1,7
+!       TRANSF_PLANE%I(IPLAN)=2
+!     ENDDO
+!     TRANSF_PLANE%I(4)=3
+!     ZPLANE%R(4)=3.D0
+!     ZSTAR%R(2)=0.2D0
+!     ZSTAR%R(3)=0.8D0
+!     ZSTAR%R(5)=0.1D0
+!     ZSTAR%R(6)=0.9D0
+!
 !
 !***********************************************************************
 !
-      CALL CALCOT(Z,H%R)
+!     COMPUTES ELEVATIONS
+!     IF IT IS A CONTINUATION, WILL BE DONE AFTER CALLING 'SUITE'
+!
+      IF(DEBU) CALL CALCOT(Z,H%R)
 !
 !***********************************************************************
 !
-!     INITIALISATION DES VITESSES
+!     INITIALISES VELOCITIES
 !
-      CALL OS( 'X=C     ' , U , U , U , 0.D0 )
-      CALL OS( 'X=C     ' , V , V , V , 0.D0 )
-      CALL OS( 'X=C     ' , W , W , W , 0.D0 )
+      IF(SUIT2) THEN
+        DO I=1,NPLAN
+          DO J=1,NPOIN2
+           U%R((I-1)*NPOIN2+J)=U2D%R(J)
+           V%R((I-1)*NPOIN2+J)=V2D%R(J)
+          ENDDO
+        ENDDO
+      ELSE
+        CALL OS( 'X=0     ' , X=U )
+        CALL OS( 'X=0     ' , X=V )
+      ENDIF
+!
+      CALL OS( 'X=0     ' , X=W )
 !
 !-----------------------------------------------------------------------
 !
-!     INITIALISATION DES TRACEURS 
+!     INITIALISES TRACERS
 !
       IF (NTRAC.NE.0) THEN
-         CALL OS( 'X=C     ' , TA , TA ,TA , 0.D0 )
-!              
-         DO 21 IPLAN=1,NPLAN
-            DO 20 IPOIN2=1,NPOIN2
+        DO I=1,NTRAC
+          CALL OS( 'X=C     ', X=TA%ADR(I)%P, C=TRAC0(I))
+        ENDDO        
+        DO 21 IPLAN=1,NPLAN
+          DO 20 IPOIN2=1,NPOIN2
             IPOIN3 = IPOIN2 + (IPLAN-1)*NPOIN2
-            IF (X(IPOIN3).LE.15.D0)  TA%ADR(1)%P%R(IPOIN3) = 1.0D0
-20          CONTINUE
-21       CONTINUE
+            IF(X(IPOIN3).LE.15.D0) TA%ADR(1)%P%R(IPOIN3) = 1.D0
+20        CONTINUE
+21      CONTINUE
       ENDIF
+!
+!
+!-----------------------------------------------------------------------
+!   INITIALISES THE K-EPSILON MODEL (OPTIONAL)
+!   WHEN DONE: AKEP = .FALSE.
+!
+      AKEP=.TRUE.
+!
+!     IF(ITURBV.EQ.3) THEN
+!
+!       HERE INITIALISES K AND EPSILON
+!
+!       AKEP = .FALSE.
+!     ENDIF
 !
 !-----------------------------------------------------------------------
 !
-! INITIALIZE THE PRESSURE FIELDS TO 0.0
-! 
+! INITIALISES THE PRESSURE FIELDS TO 0.0
 !
       IF(NONHYD) THEN
         CALL OS('X=C     ',X=DP,C=0.D0)
@@ -104,4 +267,3 @@
 !
       RETURN
       END
-
