972e615b4453d72cdbab74167ccbf688b64fe01e
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2012-05-29 16:07:51 +0000

    mise a jour cas-test uneven
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@1429 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: b7c1b4ba05253b3499afd52d5d8103bbc1d21d39

--- telemac3d/tel3d_v6p2/validation/011_uneven/t3d_uneven.f
+++ telemac3d/tel3d_v6p2/validation/011_uneven/t3d_uneven.f
@@ -57,21 +57,81 @@
 !
       CALL OS ('X=Y     ', HN, H, H, 0.0D0)
 !
-!-Mesh Initialization
-      IF (NPLINT.GE.2) THEN
-        CALL OV( 'X=C     ' , Z((NPLINT-1)*NPOIN2+1 : NPLINT*NPOIN2),
-     &                Z, Z, COTINT , NPOIN2)
-      ENDIF
 !
-      IF (NPLINT.GE.2) THEN
-        DO IPLAN = 1,NPLINT-1
-          ZSTAR%R(IPLAN) = DBLE(IPLAN-NPLINT)/DBLE(NPLINT-1)
-        ENDDO
-      ENDIF
+!-----------------------------------------------------------------------
 !
-      DO IPLAN = NPLINT,NPLAN
-        ZSTAR%R(IPLAN) = DBLE(IPLAN-NPLINT)/DBLE(NPLAN-NPLINT)
+!     DATA TO BUILD VERTICAL COORDINATES IN CALCOT
+!
+!     TRANSF IS KEYWORD "MESH TRANSFORMATION"
+!     IF TRANSF = 0, SUBROUTINE CALCOT MUST BE IMPLEMENTED BY THE USER
+!
+!     AN EQUIVALENT OF TRANSF MUST BE GIVEN FOR EVERY PLANE:
+!
+!     POSSIBLE VALUES OF TRANSF_PLANE :
+!
+!     1 : SIGMA TRANSFORMATION WITH EVENLY SPACED PLANES
+!     2 : SIGMA TRANSFORMATION WITH PROPORTIONS GIVEN IN ZSTAR
+!     3 : PRESCRIBED ELEVATION GIVEN IN ZPLANE
+!
+!     STANDARD BELOW IS: EVENLY SPACED PLANES, NO OTHER DATA REQUIRED
+!
+      DO IPLAN = 1,NPLAN
+        TRANSF_PLANE%I(IPLAN)=1
       ENDDO
+!
+!     OTHER EXAMPLES:
+!
+!     EXAMPLE 1: ALL PLANES WITH PRESCRIBED ELEVATION
+!
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=3
+!     ENDDO
+!     ZPLANE%R(2)=-7.D0
+!     ZPLANE%R(3)=-4.D0
+!     ...
+!     ZPLANE%R(NPLAN-1)=-0.05D0
+!
+!
+!     EXAMPLE 2: SIGMA TRANSFORMATION WITH GIVEN PROPORTIONS
+!
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=2
+!     ENDDO
+!     ZSTAR%R(2)=0.02D0
+!     ZSTAR%R(3)=0.1D0
+!     ...
+!     ZSTAR%R(NPLAN-1)=0.95D0
+!
+!
+!     EXAMPLE 3: ONE PLANE (NUMBER 4) WITH PRESCRIBED ELEVATION
+!                AND SIGMA ELSEWHERE
+!
+!     DO IPLAN = 1,NPLAN
+!       TRANSF_PLANE%I(IPLAN)=1
+!     ENDDO
+!     TRANSF_PLANE%I(4)=3
+!     ZPLANE%R(4)=-3.D0
+!
+!
+!     EXAMPLE 4: ONE PLANE WITH PRESCRIBED ELEVATION
+!                AND 2 SIGMA TRANSFORMATIONS, WITH NPLAN=7
+!                SIGMA TRANSFORMATIONS ARE MEANT BETWEEN
+!                BOTTOM, FIXED ELEVATION PLANES AND FREE SURFACE
+!                THE VALUES OF ZSTAR ARE LOCAL FOR EVERY
+!                SIGMA TRANSFORMATION: 0. FOR LOWER FIXED PLANE
+!                                      1. FOR UPPER FIXED PLANE
+!
+!     DO IPLAN = 1,7
+!       TRANSF_PLANE%I(IPLAN)=2
+!     ENDDO
+!     TRANSF_PLANE%I(4)=3
+!     ZPLANE%R(4)=3.D0
+!     ZSTAR%R(2)=0.2D0
+!     ZSTAR%R(3)=0.8D0
+!     ZSTAR%R(5)=0.1D0
+!     ZSTAR%R(6)=0.9D0
+!
+!
 !
       CALL CALCOT(Z, H%R)
 !
@@ -88,17 +148,17 @@
 !
           IPOIN3 = (IPLAN-1)*NPOIN2 + IPOIN2
 !
-          HV1=SQRT(3.0D0/4.0D0*WH/WX**3.0D0)*(X(IPOIN3)-X0)
+          HV1=SQRT(3.0D0/4.0D0*WH/WX**3)*(X(IPOIN3)-X0)
           HV2=2.0D0/(EXP(HV1)+EXP(-HV1))
-          U%R(IPOIN3)=SQRT(GRAV*WX)*WH/WX*HV2**2.0D0
+          U%R(IPOIN3)=SQRT(GRAV*WX)*WH/WX*HV2**2
 
           HV3=(EXP(HV1)-EXP(-HV1))/(EXP(HV1)+EXP(-HV1))
           HV4=SQRT(3.0D0*GRAV*WX)
-     &       *((SQRT(WH/WX))**3.0D0)*(Z(IPOIN3)+WX)/WX
-          W%R(IPOIN3)=(HV4*HV2**2.0D0)*HV3
+     &       *((SQRT(WH/WX))**3)*(Z(IPOIN3)+WX)/WX
+          W%R(IPOIN3)=(HV4*HV2**2)*HV3
 !
-        END DO
-      END DO
+        ENDDO
+      ENDDO
 !
 !
 ! INITIALIZE THE PRESSURE FIELDS TO 0.0
@@ -112,9 +172,9 @@
 !
       RETURN
       END
-!                       *****************
-                        SUBROUTINE CORFON
-!                       *****************
+!                       *********************
+                        SUBROUTINE T3D_CORFON
+!                       *********************
 !
      &(SZF, ST1, ST2, ZF, T1, T2, X, Y, PRIVE, NPOIN2,
      & LISFON, MSK, MASKEL, MATR2D, MESH2D, S)
