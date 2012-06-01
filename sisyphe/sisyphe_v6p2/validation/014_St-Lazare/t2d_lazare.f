151f0bc70757ba727a52ff8300d0522e1dc98a7e
p.tassi <p.tassi@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2012-06-01 09:23:01 +0000

    added example "St-Lazare" (coupled t2d + sis)
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@1471 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 61067568235a192c98968667eb9026abb87f54ac

---
+++ sisyphe/sisyphe_v6p2/validation/014_St-Lazare/t2d_lazare.f
@@ -0,0 +1,77 @@
+C                       *****************
+                        SUBROUTINE CORFON
+C                       *****************
+C
+C***********************************************************************
+C TELEMAC 2D VERSION 5.2          01/03/90    J-M HERVOUET
+C***********************************************************************
+C
+C  USER SUBROUTINE CORFON
+C
+C  FUNCTION  : MODIFICATION OF THE BOTTOM TOPOGRAPHY
+C
+C
+C-----------------------------------------------------------------------
+C  ARGUMENTS USED IN THE EXAMPLE 
+C .________________.____.______________________________________________
+C |      NOM       |MODE|                   ROLE
+C |________________|____|_______________________________________________
+C |      ZF        |<-->| FOND A MODIFIER.
+C |      X,Y,(Z)   | -->| COORDONNEES DU MAILLAGE (Z N'EST PAS EMPLOYE).
+C |      A         |<-- | MATRICE
+C |      T1,2      | -->| TABLEAUX DE TRAVAIL (DIMENSION NPOIN)
+C |      W1        | -->| TABLEAU DE TRAVAIL (DIMENSION 3 * NELEM)
+C |      NPOIN     | -->| NOMBRE DE POINTS DU MAILLAGE.
+C |      PRIVE     | -->| TABLEAU PRIVE POUR L'UTILISATEUR.
+C |      LISFON    | -->| NOMBRE DE LISSAGES DU FOND.
+C |________________|____|______________________________________________
+C MODE : -->(DONNEE NON MODIFIEE), <--(RESULTAT), <-->(DONNEE MODIFIEE)
+C-----------------------------------------------------------------------
+C
+C PROGRAMME APPELANT :
+C PROGRAMMES APPELES : RIEN EN STANDARD
+C
+C***********************************************************************
+C
+      USE BIEF
+      USE DECLARATIONS_TELEMAC2D
+C
+      IMPLICIT NONE
+      INTEGER LNG,LU
+      COMMON/INFO/LNG,LU
+C
+C+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+C
+C
+C+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+C
+      LOGICAL MAS
+C
+C-----------------------------------------------------------------------
+C
+C  LISSAGES EVENTUELS DU FOND
+C
+      IF(LISFON.GT.0) THEN
+C
+        MAS=.TRUE.
+        CALL FILTER(ZF,MAS,T1,T2,AM1,'MATMAS          ',
+     *              1.D0,T1,T1,T1,T1,T1,T1,MESH,MSK,MASKEL,LISFON)
+C
+      ENDIF
+C
+C-----------------------------------------------------------------------
+C CCV MODIFICATION DU FOND FRONTIERE 2
+C ++++++++++++++++++++++++++++++++++++
+      ZF%R(61) =460.5
+      ZF%R(323)=459.5
+      ZF%R(565)=459.5
+      ZF%R(363)=460.5
+  	
+C-----------------------------------------------------------------------
+C
+      RETURN
+      END                  
+C                       *****************
+   
+ 
+      
