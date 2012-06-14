3f2ae841a656dafce9ec63cf9d46a702e8d476b8
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2012-06-14 12:42:29 +0000

    Telemac-2D: correction in call to siphon, dsce instead of dsce2
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/tags/v6p1@1513 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: d1cfc9255650402a5b0ff59028e217b45261d0df

--- telemac2d/tel2d_v6p1/sources/debimp.f
+++ telemac2d/tel2d_v6p1/sources/debimp.f
@@ -86,8 +86,6 @@
 !  THE LAST NODE ON THE BOUNDARY. IN FACT THIS SEGMENT WILL BE SOLID
 !  AND WILL HAVE A MASK ALREADY SET TO ZERO.
 !
-      IF(EQUA(1:15).NE.'SAINT-VENANT VF') THEN
-!
       DO K=1,NPTFR
         IF(NUMLIQ(K).EQ.IFRLIQ) THEN
           WORK1%R(K)=MASK(K)
@@ -95,24 +93,6 @@
           WORK1%R(K)=0.D0
         ENDIF
       ENDDO
-!
-      ELSE
-!
-!     FINITE VOLUMES COUNT THE SOLID SEGMENTS CLOSE TO
-!     THE LIQUID BOUNDARIES
-!
-      DO K=1,NPTFR
-        IF(NUMLIQ(K).EQ.IFRLIQ) THEN
-          WORK1%R(K)          =MASK(K)
-!         RA ON 07/12/2010
-!         WORK1%R(KP1BOR(K,1))=MASK(K)
-!         WORK1%R(KP1BOR(K,2))=MASK(K)
-        ELSE
-          WORK1%R(K)=0.D0
-        ENDIF
-      ENDDO
-!
-      ENDIF
 !
       IELM=11
       CALL VECTOR(WORK2,'=','FLUBDF          ',IELBOR(IELM,1),
