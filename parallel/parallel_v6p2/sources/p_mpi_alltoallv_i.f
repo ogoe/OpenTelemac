397c1dac39c4c2124e69884d83dd398f40015928
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2012-08-02 06:59:01 +0000

    parallel: adding alltoallv for integers
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@1917 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: d618e93183a813b110e69d3bf5e9bee1ed98004b

---
+++ parallel/parallel_v6p2/sources/p_mpi_alltoallv_i.f
@@ -0,0 +1,67 @@
+!                    ****************************
+                     SUBROUTINE P_MPI_ALLTOALLV_I
+!                    ****************************
+!
+     &(I1,I2,I3,I4,I5,I6,I7,I8,I9,I10)
+!
+!***********************************************************************
+! PARALLEL   V6P2                                  21/08/2010
+!***********************************************************************
+!
+!brief    CALLS FUNCTION MPI_ALLTOALLV FOR INTEGERS.
+!
+!history  C. DENIS (SINETICS)
+!+        27/10/2009
+!+        V6P0
+!+
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        13/07/2010
+!+        V6P0
+!+   Translation of French comments within the FORTRAN sources into
+!+   English comments
+!
+!history  N.DURAND (HRW), S.E.BOURBAN (HRW)
+!+        21/08/2010
+!+        V6P0
+!+   Creation of DOXYGEN tags for automated documentation and
+!+   cross-referencing of the FORTRAN sources
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!| I1             |-->| SEND BUFFER  
+!| I2             |-->| SPECIFIES THE NUMBER OF ELEMENTS TO SEND TO EACH
+!|                |   | PROCESSOR 
+!| I3             |-->| DISPLACEMENT ARRAY FOR THE SEND BUFFER 
+!| I4             |-->| DATA TYPE OF SEND BUFFER ELEMENTS
+!| I5             |<--| RECEIVE BUFFER
+!| I6             |-->| SPECIFIES THE MAXIMUM NUMBER OF ELEMENTS THAT 
+!|                |   | CAN BE RECEIVED FROM EACH PROCESSOR
+!| I7             |-->| DISPLACEMENT ARRAY FOR THE RECEIVE BUFFER 
+!| I8             |-->| DATA TYPE OF RECEIVE BUFFER ELEMENTS
+!| I9             |-->| COMMUNICATOR 
+!| I10            |-->| ERROR VALUE 
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
+      IMPLICIT NONE
+      INTEGER LNG,LU
+      COMMON/INFO/LNG,LU
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      INTEGER, INTENT(IN)  :: I1(*),I2(*),I3(*),I4,I6(*),I7(*),I8,I9
+      INTEGER, INTENT(OUT) :: I5(*),I10
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      CALL MPI_ALLTOALLV(I1,I2,I3,I4,I5,I6,I7,I8,I9,I10)
+!
+      IF(I10.NE.0) THEN
+        WRITE(LU,*) 'P_MPI_ALLTOALLV_I:'
+        WRITE(LU,*) 'MPI ERROR ',I10
+        STOP
+      ENDIF
+!
+!-----------------------------------------------------------------------
+!
+      RETURN
+      END
