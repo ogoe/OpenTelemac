e4041e03dbf014ba293adcb9bdad58734f60ff3f
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2013-03-22 16:28:13 +0000

    sisyphe: cleaning
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@3124 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 75b7a9b73def795bbc3fcdbe72a875529380035b

--- sisyphe/sisyphe_v6p2/sources/cvsp_p.f
+++ sisyphe/sisyphe_v6p2/sources/cvsp_p.f
@@ -1,7 +1,8 @@
-!                    ****************************************
-                     SUBROUTINE CVSP_P (PATH_PRE,FILE_PRE,JG)
-!                    ****************************************
+!                        *****************
+                         SUBROUTINE CVSP_P
+!                        *****************
 !
+     &(PATH_PRE,FILE_PRE,JG)
 !
 !***********************************************************************
 ! SISYPHE   V6P3                                   14/03/2013
@@ -40,57 +41,54 @@
 !
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 !
-      CHARACTER*100, DEBUGFILE
-      CHARACTER*5, OCSTR
-      INTEGER  I, K, J, J2
-      DOUBLEPRECISION AT, BSUM, LEVELBELOW
+      CHARACTER(LEN=100) DEBUGFILE
+      CHARACTER(LEN=5) OCSTR
+      INTEGER I, K, J
+      DOUBLE PRECISION AT, BSUM
 !
 !----------------------------------------------------------------------- 
 !
       AT = DT*LT/PERCOU
       J = JG
       OUTPUTCOUNTER = OUTPUTCOUNTER + 1
-      
+!      
 !     GLOBAL NUMBERS TO GLOBAL NUMBERS
-      IF (NCSIZE > 1) THEN
-         J = MESH%KNOGL%I(JG)
+      IF(NCSIZE > 1) THEN
+        J = MESH%KNOGL%I(JG)
       ENDIF
-      
-      WRITE(UNIT=OCSTR, FMT='(I5)')
-     &     OUTPUTCOUNTER
+!      
+      WRITE(UNIT=OCSTR, FMT='(I5)') OUTPUTCOUNTER
       DO I=1,5
-         IF(OCSTR(I:I)==' ') OCSTR(I:I)='0'
-      END DO
-      
+        IF(OCSTR(I:I)==' ') OCSTR(I:I)='0'
+      ENDDO
+!      
       WRITE(UNIT=DEBUGFILE, FMT='(A,A,A,A,I8,A,G15.8,A)')
      &     PATH_PRE,OCSTR,'_',FILE_PRE,
      &     JG,'_T_',AT,'.VSP.CSV'
 
       DO I=1,LEN_TRIM(DEBUGFILE)
-         IF(DEBUGFILE(I:I)==' ') DEBUGFILE(I:I)='_'
-      END DO
-      
-      
+        IF(DEBUGFILE(I:I)==' ') DEBUGFILE(I:I)='_'
+      ENDDO
+!            
       IF(J > 0) THEN
-         OPEN(81, FILE=DEBUGFILE, STATUS='UNKNOWN' )
-         REWIND 81
+        OPEN(81, FILE=DEBUGFILE, STATUS='UNKNOWN' )
+        REWIND 81
        WRITE(81,*)"J K FD50(I) AT PRO_D(K_I) PRO_F(K_I) X Y D50 ALT T H"
-         
+!         
        DO K=1,PRO_MAX(J)
           BSUM = 0.D0
           DO I=1,NSICLA
              BSUM = FDM(I)*PRO_F(J,PRO_MAX(J)+1-K,I) + BSUM
           ENDDO
-          
+!          
           DO I=1,NSICLA
-             IF (K.EQ.1) THEN
+             IF(K.EQ.1) THEN
 ! FULL OUTPUT WITH COORDINATES ETC. ON SURFACE
                 WRITE (81,'(I8,1X,I4,1X,10(G20.12,1X))')
      &               JG,PRO_MAX(J)+1-K,FDM(I),AT,
      &               PRO_D(J,PRO_MAX(J)+1-K,I),
      &               PRO_F(J,PRO_MAX(J)+1-K,I),X(J),Y(J),
-     &               BSUM,ES(J,1),TOB%R(J), Z%R(J) !UHM
-                  
+     &               BSUM,ES(J,1),TOB%R(J), Z%R(J) !UHM                  
              ELSE
 ! FOLLOWING SECTIONS
                 WRITE (81,'(I8,1X,I4,1X,5(G20.12,1X))')
@@ -100,14 +98,14 @@
              ENDIF
           ENDDO
        ENDDO
-       
+!       
        BSUM = 0.D0
        DO I=1,NSICLA
-          BSUM = FDM(I)*PRO_F(J,1,I) + BSUM
+         BSUM = FDM(I)*PRO_F(J,1,I) + BSUM
        ENDDO
-       
+!       
        CLOSE(81)
-       
+!       
       ENDIF
 !
 !-----------------------------------------------------------------------
