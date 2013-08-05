b186f0962dbf5d8fea7e8d2699b452e1aecc1985
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2013-08-05 08:18:57 +0000

    sisyphe, cvsp case: tentative fix to Python problem, undefined reference subroutines added in the Fortran of the case.
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@3973 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 44e3bffceb8f6a8a20a946d4a7dcb01701406062

--- examples/sisyphe/cvsp/sis_cvsp.f
+++ examples/sisyphe/cvsp/sis_cvsp.f
@@ -1,3 +1,11 @@
+!
+!
+!  cvsp_init_from_layers and cvsp_output_init have been added here
+!  as a fix to the Python scan problem: they are not called by the
+!  standard Sisyphe, thus not compiled
+!
+!
+!
 ! global variable for recirculation per class 
         MODULE RECIRCMODUL 
 !              use declarations_sisyphe :: nsicla 
@@ -66,9 +74,9 @@ C RIGID BEDS POSITION
 C---------------------
 C
 C       DEFAULT VALUE:       ZR=ZF-100
-c
+!
         CALL OV( 'X=Y+C     ',ZR,ZF,ZF,-0.3D0,NPOIN)
-
+!
         DO i=1,NPOIN
         IF (X(i) < 100.0D0) THEN
            ! zr(i)=zf(i)
@@ -153,8 +161,7 @@ C
 C----------------------------------------------------------------------- 
 C 
       DO J=1,NPOIN 
- 
- 
+! 
             NCOUCHES(J) = NOMBLAY
             NLAYER%I(J) = NOMBLAY
 
@@ -169,10 +176,9 @@ C
             ES(J,8) = 0.01D0
 
             ES(J,9) = (ZF%R(J) -ZR%R(J)-(NOMBLAY-2)*0.01D0 - ES(J,1))
-
+!
           BLOMCASE = 'B1'
-
- 
+! 
           DO I = 1, NSICLA 
           DO K = 1, NCOUCHES(J) 
 C        Nur für BLOOM B CASES 
@@ -197,69 +203,36 @@ C        Nur für BLOOM B CASES
  
  
       ENDDO 
-C 
-C----------------------------------------------------------------------- 
-C  !CONVERT TO CVSM in CASE VSMTYPE = 1
-C  No user edit necessary
-C-----------------------------------------------------------------------
-
-
-
-      IF (VSMTYPE.eq.1) THEN
-
-        !Folder for Debugging Outputs
-!        CALL system('mkdir ./ERR')
-!        CALL system('cp ../../vsp2svg.exe ./ERR')
-!        CALL system('cp ../../*.css ./ERR')
-!        CALL system('cp ../../*.svg ./ERR')
-
-
-        !Folder for Hirano Profile Outputs
-!        CALL system('mkdir ./LAY')
-!        CALL system('cp ../../vsp2svg.exe ./LAY')
-!        CALL system('cp ../../*.css ./LAY')
-!        CALL system('cp ../../*.svg ./LAY')
-
-
+! 
+!----------------------------------------------------------------------- 
+!  !CONVERT TO CVSM in CASE VSMTYPE = 1
+!  No user edit necessary
+!-----------------------------------------------------------------------
+!
+      IF(VSMTYPE.eq.1) THEN
+!        Folder for Hirano Profile Outputs
          do kk = 1,100
-         if (CVSMOUTPUT(kk).gt.0) THEN
-                call LAYERS_P('./LAY/VSP_',  CVSMOUTPUT(kk))
+         if(CVSMOUTPUT(kk).gt.0) THEN
+           call LAYERS_P('./VSP_',CVSMOUTPUT(kk))
          endif
          enddo
-
-C
-
-        !Folder for CVSP Profile Outputs
-!        CALL system('mkdir ./VSP')
-!        CALL system('cp ../../vsp2svg.exe ./VSP')
-!       CALL system('cp ../../*.css ./VSP')
-!        CALL system('cp ../../*.svg ./VSP')
-
-        !Copy TECPLOT Layouts to the Targetfolder
-!        CALL system('cp ../*.lay .')
-
-
-        !call CVSP_INIT()
-         call CVSP_INIT_from_LAYERS()
-
-        !output to Selafin file
-         if (CVSM_OUT_FULL) call CVSP_OUTPUT_INIT()
-            if (CVSM_OUT_FULL) call CVSP_WRITE_PROFILE()
-
+         call CVSP_INIT_from_LAYERS
+!        output to Selafin file
+         if (CVSM_OUT_FULL) THEN
+           call CVSP_OUTPUT_INIT
+           call CVSP_WRITE_PROFILE
+         ENDIF
          do kk = 1,100
          if (CVSMOUTPUT(kk).gt.0) THEN
-                call CVSP_P('./','V_', CVSMOUTPUT(kk))
+           call CVSP_P('./','V_', CVSMOUTPUT(kk))
          endif
          enddo
-
-      ENDIF ! END CVSM
-
- 
+      ENDIF ! END CVSM 
       RETURN 
       END SUBROUTINE INIT_COMPO
-!                    *****************
+!                    *********************
                      SUBROUTINE CONLIT_UWE
-!                    *****************
+!                    *********************
 !
      &(NBOR,AT)
 !
@@ -348,11 +321,7 @@ C
               Endif ! FLUX for one class
           endif
       ENDDO
-
-                !print*,'Inflowwidth: ',Inflowwidth
-
-
-
+!
       DO  K=1,NPTFR
 !
         I = NBOR(K)
@@ -441,3 +410,308 @@ C
 !
       RETURN
       END SUBROUTINE
+!                    ********************************
+                     SUBROUTINE CVSP_INIT_FROM_LAYERS
+!                    ********************************
+!
+!
+!***********************************************************************
+! SISYPHE V6P3                                   14/03/2013
+!***********************************************************************
+!
+!brief   INITS A VERTICAL SORTING PROFILE USING HIRANO LAYERS
+!
+!history UWE MERKEL
+!+        2012-04-19
+!+
+!history  P. A. TASSI (EDF R&D, LNHE)
+!+        12/03/2013
+!+        V6P3
+!+   Cleaning, cosmetic
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!|           |---|
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
+      USE DECLARATIONS_TELEMAC
+      USE DECLARATIONS_SISYPHE
+!
+      IMPLICIT NONE
+!
+      INTEGER  I, J, K, M, L, MDISC, UBS
+      DOUBLE PRECISION DEPTH
+!
+!----------------------------------------------------------------------- 
+!
+      ALLOCATE(PRO_D(NPOIN,PRO_MAX_MAX,NSICLA))
+      ALLOCATE(PRO_F(NPOIN,PRO_MAX_MAX,NSICLA))
+      ALLOCATE(PRO_MAX(NPOIN))
+!
+!----------------------------------------------------------------------- 
+!
+      DO J=1,NPOIN
+         DEPTH = 0                    ! INIT DEPTH OF THE VSP
+         PRO_MAX(J) =  2* NLAYER%I(J) ! 2 SECTION POINTS PER LAYER
+         L = PRO_MAX(J)
+!
+!-----------------------------------------------------------------------     
+! WATER / BOTTOM
+!-----------------------------------------------------------------------
+!
+         DO I=1,NSICLA
+            PRO_D(J,L,I) = ZF%R(J)
+            PRO_F(J,L,I) = AVAIL(J,1,I)
+         ENDDO
+!
+!-----------------------------------------------------------------------     
+! SECTIONS 
+!-----------------------------------------------------------------------
+!
+         DO M=1,NLAYER%I(J)-1   !FOR THE UPPER 8 LAYERS
+            DEPTH = DEPTH + ES(J,M)
+            L = L - 1
+            DO I=1,NSICLA
+               PRO_D(J,L,I) = ZF%R(J) - DEPTH
+               PRO_F(J,L,I) = AVAIL(J,M,I)
+            ENDDO
+            L = L - 1
+            DO I=1,NSICLA
+               PRO_D(J,L,I) = ZF%R(J) - DEPTH
+               PRO_F(J,L,I) = AVAIL(J,M+1,I)
+            ENDDO
+         ENDDO
+!
+!-----------------------------------------------------------------------     
+! BOTTOM / RIGID BED
+!-----------------------------------------------------------------------
+!         
+         L = L - 1
+         DO I=1,NSICLA
+            PRO_D(J,L,I) = ZR%R(J)
+            PRO_F(J,L,I) = AVAIL(J,NLAYER%I(J),I)
+         ENDDO
+!
+         CALL CVSP_COMPRESS_DP(J,1.D-5)
+!
+      ENDDO
+!
+!-----------------------------------------------------------------------
+!
+      RETURN
+      END SUBROUTINE CVSP_INIT_FROM_LAYERS
+!                      ***************************
+                       SUBROUTINE CVSP_OUTPUT_INIT
+!                      ***************************
+!
+!
+!***********************************************************************
+! SISYPHE   V6P3                                   14/03/2013
+!***********************************************************************
+!
+!brief    CVSP_OUTPUT_INIT
+!
+!history  U.MERKEL
+!+        30/07/2012
+!+        V6P2
+!+
+!history  P. A. TASSI (EDF R&D, LNHE)
+!+        12/03/2013
+!+        V6P3
+!+   Cleaning, cosmetic
+!
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!|                |<->| 
+!|                |-->| 
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
+      USE BIEF
+      USE DECLARATIONS_SISYPHE
+      USE CVSP_OUTPUTFILES
+
+      IMPLICIT NONE
+
+      CHARACTER*32 VLABEL
+      CHARACTER(LEN=11) :: EXTENS
+      INTEGER I, K
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      INTEGER CFG(2)            ! FOR 3D MESH
+      CFG(1) = OPTASS
+      CFG(2) = PRODUC           ! PRODUC=1 HARD IN LECDON
+!
+!-----------------------------------------------------------------------     
+! CHECKPOINTFILE
+!-----------------------------------------------------------------------
+!
+      CP_FILES(3)%FMT = 'SERAFIN ' !'SERAFIN ' OR 'SERAFIND'
+      CP_FILES(3)%LU = 10004    !INTEGER, INDIVIDUAL
+      CP_FILES(3)%NAME = 'SORTINGPROFILE.RES' !UP TO 144 CHARACTERS
+      CP_FILES(3)%TELNAME = 'VSPRES' !ONLY 6 CHARACTERS
+      CP_FILES(3)%ACTION = 'WRITE'
+      USRMSH_NPLAN = PRO_MAX_MAX
+      
+      CP_FILES(4)%FMT = 'SERAFIN ' !'SERAFIN ' OR 'SERAFIND'
+      CP_FILES(4)%LU = 10005    !INTEGER, INDIVIDUAL
+      CP_FILES(4)%NAME = '2DHYDROAS3D.RES' !UP TO 144 CHARACTERS
+      CP_FILES(4)%TELNAME = 'VSPHYD' !ONLY 6 CHARACTERS
+      CP_FILES(4)%ACTION = 'WRITE'
+      USRMSH_2DHYD_NPLAN = 2
+!
+!-----------------------------------------------------------------------     
+! ALLOCATES A 3D MESH FOR USEROUTPU: HERE VSPRES
+!-----------------------------------------------------------------------
+!
+      CALL ALMESH(USRMSH,'USRMSH',41,SPHERI,CFG,SIS_FILES(SISGEO)%LU,
+     &     EQUA,NPLAN=USRMSH_NPLAN,
+     &     FILE_FORMAT=SIS_FILES(SISGEO)%FMT)
+      
+      DO I =1,USRMSH%NPTFR
+         USRMSH%NBOR%I(I) = I
+      END DO
+!     
+!-----------------------------------------------------------------------     
+! ALLOCATES A 3D MESH FOR USEROUTPU: HERE VSPHYD
+!-----------------------------------------------------------------------
+!
+      CALL ALMESH(USRMSH_2DHYD,'USRHYD',41,SPHERI,CFG,
+     &     SIS_FILES(SISGEO)%LU,
+     &     EQUA,NPLAN=USRMSH_2DHYD_NPLAN,
+     &     FILE_FORMAT=SIS_FILES(SISGEO)%FMT)
+      
+      DO I =1,USRMSH_2DHYD%NPTFR
+         USRMSH_2DHYD%NBOR%I(I) = I
+      END DO
+!
+!-----------------------------------------------------------------------     
+! SET HERE THE NAMES OF THE PRINTOUT VARIABLES FOR THE USER FILE VSPRES
+!-----------------------------------------------------------------------
+!
+      DO I = 1,NUMVARUR3D2RES
+         IF (I.LE.3+NSICLA) THEN
+            UR3D_FILES_OUTVAR(I) = .TRUE.
+         ELSE
+            UR3D_FILES_OUTVAR(I) = .FALSE.
+         ENDIF
+         WRITE(UNIT=VLABEL, FMT='(A5,I2,A25)') 'FRACTION_CLASS_',I-3
+         UR3D_FILES_LABELS(I) = VLABEL
+      ENDDO
+! EXAMPLE FOR MANUALLY OVERWRITTING LEN=32
+      UR3D_FILES_LABELS(1)= 'Z [M]                           ' !'PROFILE_ELEVATION               '
+      UR3D_FILES_LABELS(2)= 'PROFILE_D50 [M]                 '
+      UR3D_FILES_LABELS(3)= 'PROFILE_ERROR [-]               '
+!
+!-----------------------------------------------------------------------     
+! SET HERE THE NAMES OF THE PRINTOUT VARIABLES FOR THE USER FILE VSPHYD
+!-----------------------------------------------------------------------
+!
+      DO I = 1,NUMVAR2DHYD
+         UR2DHYD_FILES_OUTVAR(I) = .TRUE.
+      ENDDO
+      
+      UR2DHYD_FILES_LABELS(1)= 'Z [M]                           ' !'Z [M] AND ZF [M]                '
+      UR2DHYD_FILES_LABELS(2)= 'U [M/S]                         '
+      UR2DHYD_FILES_LABELS(3)= 'V [M/S]                         '
+      UR2DHYD_FILES_LABELS(4)= 'W [M/S]                         '
+      UR2DHYD_FILES_LABELS(5)= 'SCALAR VELOCITY [M/S]           '
+      UR2DHYD_FILES_LABELS(6)= 'TAU [N/M**2]                    '
+!     
+!-----------------------------------------------------------------------     
+! ALLOCATES THE BLOCK CONNECTING A VARIABLE NAME TO ITS ARRAY  
+!-----------------------------------------------------------------------
+!
+      CALL ALLBLO(URBLOC3D, 'URBL3D')
+      CALL ALLBLO(URBLOC2DHYD, 'URB2DH')
+!
+!-----------------------------------------------------------------------     
+! OPENS THE FILES FOR WRITING
+!-----------------------------------------------------------------------
+!
+      DO I = 3, 4
+         IF(NCSIZE.LE.1) THEN
+! SCALAR
+            OPEN(CP_FILES(I)%LU,FILE=CP_FILES(I)%TELNAME,
+     &           ACTION=CP_FILES(I)%ACTION,FORM='UNFORMATTED')
+         ELSE
+! PARALLEL, FILE TYPE: SCAL
+            IF(CP_FILES(I)%TYPE(1:4).EQ.'SCAL') THEN
+               OPEN(CP_FILES(I)%LU,
+     &              FILE=TRIM(CP_FILES(I)%TELNAME),
+     &              ACTION=CP_FILES(I)%ACTION,FORM='UNFORMATTED')
+! PARALLEL, OTHER FILE TYPE
+            ELSE
+               OPEN(CP_FILES(I)%LU,
+     &              FILE=TRIM(CP_FILES(I)%TELNAME)
+     &              //EXTENS(NCSIZE-1,IPID),
+     &              ACTION=CP_FILES(I)%ACTION,FORM='UNFORMATTED')
+            ENDIF
+         ENDIF
+         
+      ENDDO                     !CP_FILES
+!
+!-----------------------------------------------------------------------     
+! WRITES THE HEADER OF THE RESFILES
+!-----------------------------------------------------------------------
+!
+      CALL CREATE_DATASET_SERAFIN(CP_FILES(4)%LU,
+     &     '2D HYDRAULIC PARAMETERS IN 3D  '//
+     &     '                                         ',
+     &     NUMVAR2DHYD,
+     &     UR2DHYD_FILES_LABELS,
+     &     UR2DHYD_FILES_OUTVAR,
+     &     CP_FILES(4)%FMT)
+
+      CALL CREATE_DATASET_SERAFIN(CP_FILES(3)%LU,
+     &     'USEROUTPUT3D                   '//
+     &     '                                         ',
+     &     NUMVARUR3D2RES,
+     &     UR3D_FILES_LABELS,
+     &     UR3D_FILES_OUTVAR,
+     &     CP_FILES(3)%FMT)
+!
+!-----------------------------------------------------------------------     
+! WRITES THE MESH INFORMATION IN THE OUTPUT FILE :
+!-----------------------------------------------------------------------
+!
+      DO I = 3, 3               !UBOUND(CP_FILES)
+         CALL WRITE_MESH(CP_FILES(I)%FMT, ! RESULTS FILE FORMAT
+     &        CP_FILES(I)%LU,   ! LU FOR RESULTS FILE
+     &        USRMSH,           ! CHARACTERISES MESH
+     &        USRMSH_NPLAN,     ! NUMBER OF PLANES /NA/
+     &        MARDAT,           ! START DATE
+     &        MARTIM,           ! START TIME
+     &        I_ORIG,J_ORIG)    ! COORDINATES OF THE ORIGIN.
+      ENDDO
+
+      DO I = 4, 4               !UBOUND(CP_FILES)
+         CALL WRITE_MESH(CP_FILES(I)%FMT, ! RESULTS FILE FORMAT
+     &        CP_FILES(I)%LU,   ! LU FOR RESULTS FILE
+     &        USRMSH_2DHYD,     ! CHARACTERISES MESH
+     &        USRMSH_2DHYD_NPLAN, ! NUMBER OF PLANES /NA/
+     &        MARDAT,           ! START DATE
+     &        MARTIM,           ! START TIME
+     &        I_ORIG,J_ORIG)    ! COORDINATES OF THE ORIGIN.
+      ENDDO
+!
+!-----------------------------------------------------------------------     
+! INITS OUTPUT VECTORS
+!-----------------------------------------------------------------------
+!
+! VSPRES
+      DO K = 1, NSICLA
+         CALL BIEF_ALLVEC(1,VSP_FRA(K),'VSPFRA',41,1,1,USRMSH)
+      ENDDO
+      
+      CALL BIEF_ALLVEC(1,VSP_D,     'VSP__D',41,1,1,USRMSH)
+      CALL BIEF_ALLVEC(1,VSP_D50,   'VSPD50',41,1,1,USRMSH)
+      CALL BIEF_ALLVEC(1,VSP_ERROR, 'VSP_ER',41,1,1,USRMSH)
+      
+! VSPHYD
+      DO K = 1, NUMVAR2DHYD
+         CALL BIEF_ALLVEC(1,UR2DHYD(K),'VSPHYD',41,1,1,USRMSH_2DHYD)
+      ENDDO
+!
+!-----------------------------------------------------------------------
+!
+      RETURN
+      END
