4a0aac61c2f919eb0ced2bab0f21048bc0f4a017
y.audouin <y.audouin@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2013-08-21 07:08:54 +0000

    [all] Update of rights for files all text files set to svn:eol-style=native
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@4026 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: e5825d3ca7c3c06f1674e3d2e9b97dbca817f6bf

--- examples/stbtel/distributed_mesh/stb_distributed_mesh.sh
+++ examples/stbtel/distributed_mesh/stb_distributed_mesh.sh
@@ -1,1 +1,2 @@
-runStbtel.py r2d_malpasset-small_prim.slf -o r2d_malpasset-small_prim.med -n 4 --debug
+# Python config only script
+converter.py r2d_malpasset-small_prim.slf -o r2d_malpasset-small_prim.med -n 4 --debug
