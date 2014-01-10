58e700a031183479be4fb747402586ec96cfe94f
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2014-01-10 15:49:23 +0000

    telemac2d: test case vasque: parallelism enabled.
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@4557 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 4329f35f6bc01de441112a772b1843b88bae90e0

--- examples/telemac2d/vasque/t2d_vasque.f
+++ examples/telemac2d/vasque/t2d_vasque.f
@@ -70,11 +70,17 @@ C
       DO J=1,JM 
 C       PENTE RECTILIGNE                                                       
         POS_LOC = GLOBAL_TO_LOCAL_POINT(I+(J-1)*IM,MESH)
-        ZF%R(POS_LOC)=-0.6D0+0.46D0*FLOAT(I-1)/FLOAT(IM-1) 
-C       BOSSE GAUSSIENNE            
-        IF(I.GT.9.AND.I.LT.29) THEN
-          EIKON = -(I-19)**2/20.D0 
-          ZF%R(POS_LOC) = ZF%R(POS_LOC) + 0.1D0*EXP(EIKON)
+!
+!       NOTE JMH: THIS IS VERY HEAVY, THERE SHOULD BE A
+!                 FORMULA FUNCTION OF X.
+!
+        IF(POS_LOC.GT.0) THEN
+          ZF%R(POS_LOC)=-0.6D0+0.46D0*FLOAT(I-1)/FLOAT(IM-1) 
+C         BOSSE GAUSSIENNE            
+          IF(I.GT.9.AND.I.LT.29) THEN
+            EIKON = -(I-19)**2/20.D0 
+            ZF%R(POS_LOC) = ZF%R(POS_LOC) + 0.1D0*EXP(EIKON)
+          ENDIF
         ENDIF                                                 
       ENDDO                                                            
       ENDDO    
