2d86c61671d9b7da643e98c0e9f1882d9fe9d804
y.audouin <y.audouin@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2014-07-16 13:03:55 +0000

    Adding correction to script
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@5195 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 842ad34c0a35adadca6da808b900bc20577ed328

--- scripts/check_code.sh
+++ scripts/check_code.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-
+#
+# Author: Y. Audouin
+# Date: 16/07/2014
+#
 #Returns incorrect lines
 if [[ $# -lt 1 ]]; then
   echo "incorrect number of argument"
   echo "usage: check_code.sh path_to_code"
   exit 1
+fi
+if [[ $1 -eq "-h"]]; then
+  echo "Script checking some points of the coding convention "
+  echo "for all the .f and .F in the folder given in parameter"
+  echo "It will generate 4 files:"
+  echo "-- indent.log : contains the line where the indentation is not a 6 + x*2"
+  echo "-- comments.log : checks that the character used for comments is '!'"
+  echo "-- continuation.log : checks that the character for continuation is '&'"
+  echo "-- lowercase.log : checks that there are no lowercase code"
 fi
 #
 echo '*****************'
