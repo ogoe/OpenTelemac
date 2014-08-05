b0c903a3deed3cad2816203f8c16a4c55ef349ec
y.audouin <y.audouin@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2014-08-05 08:43:09 +0000

    [parallel] Correction in makefile + [script] Correction in check_code.sh + [examples] Correction of xml scripts
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@5272 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 89e63d031d4579655adf9b36fadc1a300fdf81c9

--- scripts/check_code.sh
+++ scripts/check_code.sh
@@ -4,12 +4,12 @@
 # Date: 16/07/2014
 #
 #Returns incorrect lines
-if [[ $# -lt 1 ]]; then
+if [ $# -lt 1 ]; then
   echo "incorrect number of argument"
   echo "usage: check_code.sh path_to_code"
   exit 1
 fi
-if [[ $1 -eq "-h"]]; then
+if [ $1 = "-h" ]; then
   echo "Script checking some points of the coding convention "
   echo "for all the .f and .F in the folder given in parameter"
   echo "It will generate 4 files:"
@@ -17,6 +17,7 @@ if [[ $1 -eq "-h"]]; then
   echo "-- comments.log : checks that the character used for comments is '!'"
   echo "-- continuation.log : checks that the character for continuation is '&'"
   echo "-- lowercase.log : checks that there are no lowercase code"
+  exit 1
 fi
 #
 echo '*****************'
