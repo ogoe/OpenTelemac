b132bd7e8db995b7ac715282afd5a3b96c50d765
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-01-13 10:24:25 +0000

    telemac3d.f : in the call to mask3d NPTFR2 is replaced by MESH2D%NELEB (these two numbers, number of boundary points in 2D and number of boundary elements, are different in parallel). mask3d : name NPTFR changed into NELEB2D (so subroutine virtually unchanged).
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@6927 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 74c0630684936ca646c31d31e0fae3c97246ef0b

--- sources/telemac3d/mask3d.f
+++ sources/telemac3d/mask3d.f
@@ -4,23 +4,13 @@
 !
      &(IFABOR3D,MASKEL,MASKPT,MASKBR,
      & X,Y,ZF,ZFE,H,HMIN,AT,LT,ITRA01,NELBO3,
-     & NELMA2,NELEM2,NPOIN2,NPTFR,NPLAN,NETAGE,IELM3,MESH2D)
+     & NELMA2,NELEM2,NPOIN2,NELEB2D,NPLAN,NETAGE,IELM3,MESH2D)
 !
 !***********************************************************************
-! TELEMAC3D   V6P1                                   21/08/2010
+! TELEMAC3D   V7P1
 !***********************************************************************
 !
 !brief
-!
-!history  JACEK A. JANKOWSKI PINXIT
-!+        **/03/99
-!+
-!+   FORTRAN95 VERSION
-!
-!history  JMH (LNHE)     ; J-M JANIN (LNH)    ; F LEPEINTRE (LNH)
-!+        21/10/08
-!+        V5P9
-!+
 !
 !history  N.DURAND (HRW), S.E.BOURBAN (HRW)
 !+        13/07/2010
@@ -33,6 +23,11 @@
 !+        V6P0
 !+   Creation of DOXYGEN tags for automated documentation and
 !+   cross-referencing of the FORTRAN sources
+!
+!history  J-M HERVOUET
+!+        13/01/2016
+!+        V7P1
+!+   Name NELEB2D changed into NELEB2D.
 !
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !| AT             |-->| TIME OF TIME STEP
@@ -54,7 +49,7 @@
 !| NETAGE         |-->| NPLAN - 1
 !| NPLAN          |-->| NUMBER OF PLANES IN THE 3D MESH OF PRISMS
 !| NPOIN2         |-->| NUMBER OF POINTS IN 2D
-!| NPTFR          |-->| NUMBER OF BOUNDARY POINTS IN 2D
+!| NELEB2D        |-->| NUMBER OF BOUNDARY ELEMENTS IN 2D
 !| X              |-->| COORDINATE
 !| Y              |-->| COORDINATE
 !| ZF             |-->| ELEVATION OF BOTTOM
@@ -69,14 +64,14 @@
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 !
       INTEGER, INTENT(IN)             :: NELEM2, NPOIN2, NETAGE, NPLAN
-      INTEGER, INTENT(IN)             :: NELMA2, NPTFR
+      INTEGER, INTENT(IN)             :: NELMA2, NELEB2D
       INTEGER, INTENT(INOUT)          :: IFABOR3D(NELEM2,5,NETAGE)
       DOUBLE PRECISION, INTENT(INOUT) :: MASKEL(NELEM2,NETAGE)
-      DOUBLE PRECISION, INTENT(INOUT) :: MASKBR(NPTFR,NETAGE)
+      DOUBLE PRECISION, INTENT(INOUT) :: MASKBR(NELEB2D,NETAGE)
       DOUBLE PRECISION, INTENT(IN)    :: X(NPOIN2), Y(NPOIN2)
       DOUBLE PRECISION, INTENT(IN)    :: ZF(NPOIN2), H(NPOIN2)
       DOUBLE PRECISION, INTENT(IN)    :: ZFE(NELEM2)
-      INTEGER, INTENT(IN)             :: NELBO3(NPTFR,NETAGE)
+      INTEGER, INTENT(IN)             :: NELBO3(NELEB2D,NETAGE)
       INTEGER, INTENT(INOUT)          :: ITRA01(NELEM2)
       INTEGER, INTENT(IN)             :: LT, IELM3
       DOUBLE PRECISION, INTENT(IN)    :: HMIN, AT
@@ -132,12 +127,12 @@
 !     INITIALISES MASKBR
 !=======================================================================
 !
-      CALL OV ( 'X=C     ',MASKBR,MASKBR,MASKBR,1.D0,NPTFR*NETAGE)
+      CALL OV ( 'X=C     ',MASKBR,MASKBR,MASKBR,1.D0,NELEB2D*NETAGE)
 !
 !     AND SETS IT WITH THE NEIGHBOURING MASKEL
 !
       CALL OVBD ('X=Y     ',MASKBR,MASKEL,MASKEL,
-     &           0.D0,NELBO3,NPTFR*NETAGE)
+     &           0.D0,NELBO3,NELEB2D*NETAGE)
 !
 !-----------------------------------------------------------------------
 !
