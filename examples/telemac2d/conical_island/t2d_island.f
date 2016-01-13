576e114eecd3a19e9431ebf2b544d80516c561fd
r.ata <r.ata@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-01-13 10:03:41 +0000

    conical island: update case and fortran files
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@6926 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 02362df5c12150bff5058145238d8fe01474272a

--- examples/telemac2d/conical_island/t2d_island.f
+++ examples/telemac2d/conical_island/t2d_island.f
@@ -92,37 +92,23 @@
 !
       RETURN
       END
-!                       ***************************
-                        SUBROUTINE PRERES_TELEMAC2D
-!                       ***************************
+!                    ***************************
+                     SUBROUTINE PRERES_TELEMAC2D
+!                    ***************************
+     &    (IMP,LEO)
+!
 !
 !***********************************************************************
-!  TELEMAC 2D VERSION 5.0    17/08/94    J-M HERVOUET (LNH) 30 87 80 18
-!
+! TELEMAC2D   V7P1
 !***********************************************************************
 !
-!     FONCTION  : PREPARATION DE VARIABLES QUI SERONT ECRITES SUR
-!                 LE FICHIER DE RESULTATS OU SUR LE LISTING.
-!
-!-----------------------------------------------------------------------
-!                             ARGUMENTS
-! .________________.____.______________________________________________.
-! |      NOM       |MODE|                   ROLE                       |
-! |________________|____|______________________________________________|
-! |      LT        | -->| NUMERO D'ITERATION
-! |________________|____|______________________________________________|
-! MODE : -->(DONNEE NON MODIFIEE), <--(RESULTAT), <-->(DONNEE MODIFIEE)
-!
-!-----------------------------------------------------------------------
-!
-!  APPELE PAR : TELMAC
-!
-!  SOUS-PROGRAMME APPELE : OV
-!
-!***********************************************************************
+!brief    PREPARES THE VARIABLES WHICH WILL BE WRITTEN TO
+!+                THE RESULTS FILE OR TO THE LISTING.
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !
       USE BIEF
       USE DECLARATIONS_TELEMAC2D
+      USE INTERFACE_TELEMAC2D
 !
       IMPLICIT NONE
       INTEGER LNG,LU
@@ -130,28 +116,162 @@
 !
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 !
+      LOGICAL ,INTENT(INOUT)::IMP,LEO
 !
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 !
-      LOGICAL IMP,LEO
+      INTEGER LTT,N,IMAX,I
 !
-      INTEGER LTT,N,IMAX
+      LOGICAL DEJA1,DEJA2
 !
       DOUBLE PRECISION HHPLG,XMAX
       DOUBLE PRECISION PI, PER, WPLG, A, B, PHI
+      DOUBLE PRECISION, PARAMETER:: EPSS=1.E-10
+      DOUBLE PRECISION GPRDTIME,LPRDTIME,RESTE
 !
       INTRINSIC MAX,SQRT
+      DATA DEJA1/.FALSE./
+      DATA DEJA2/.FALSE./
+      SAVE DEJA1,DEJA2
 !
 !-----------------------------------------------------------------------
 !
-! LOGIQUES POUR DECIDER DES SORTIES
+!
+!     THIS WILL TRIGGER THE OUTPUT OF LAST TIMESTEP
+!     BUT NOT WITH PARAMETER ESTIMATION (LISPRD WOULD STAY AT 1
+!     FOR FURTHER COMPUTATIONS)
+      IF(LT.EQ.NIT.AND.ESTIME(1:1).EQ.' ') THEN
+        LISPRD=1
+        LEOPRD=1
+      ENDIF
 !
       IMP=.FALSE.
       LEO=.FALSE.
-      LTT=(LT/LISPRD)*LISPRD
-      IF((LT.EQ.LTT.OR.LT.EQ.NIT).AND.LT.GE.PTINIL) IMP=.TRUE.
-      LTT=(LT/LEOPRD)*LEOPRD
-      IF((LT.EQ.LTT.OR.LT.EQ.NIT).AND.LT.GE.PTINIG) LEO=.TRUE.
+!     Always write the intial conditions
+      IF(LT.EQ.0) THEN
+        IMP=.TRUE.
+        LEO=.TRUE.
+        COMPLEO=0
+      ELSE
+        IF(EQUA(1:15).NE.'SAINT-VENANT VF') THEN
+!         FEM
+          LTT=(LT/LISPRD)*LISPRD
+          IF(LT.EQ.LTT.AND.LT.GE.PTINIL) IMP=.TRUE.
+          LTT=(LT/LEOPRD)*LEOPRD
+          IF(LT.EQ.LTT.AND.LT.GE.PTINIG) LEO=.TRUE.
+!         FOR GRAPHICAL OUTPUTS          
+          COMPLEO=LT
+        ELSE
+!         FVM
+          GPRDTIME=LEOPRD*DTINI
+          LPRDTIME=LISPRD*DTINI
+          IF(GPRDTIME.LT.EPSS.OR.LPRDTIME.LT.EPSS)THEN
+            CALL PLANTE(1)
+            STOP
+          ENDIF
+          IF(LT.GE.PTINIG)THEN
+!           GRAPHIC OUTPUT
+            LTT=CEILING(AT/GPRDTIME)
+            RESTE=(LTT*GPRDTIME-AT)/GPRDTIME
+            IF(RESTE.LT.EPSS.OR.ABS(RESTE-1.D0).LT.EPSS)THEN
+!                                   CASE WHERE RESTE=1
+              LEO=.TRUE.
+              COMPLEO=COMPLEO+1
+            ENDIF
+          ENDIF
+          IF(LT.GT.PTINIL)THEN
+!           LISTING OUTPUT
+            LTT=CEILING(AT/LPRDTIME)
+            RESTE=(LTT*LPRDTIME-AT)/LPRDTIME
+            IF(RESTE.LT.EPSS.OR.ABS(RESTE-1.D0).LT.EPSS)IMP=.TRUE.
+          ENDIF
+        ENDIF
+      ENDIF
+!
+!
+!-----------------------------------------------------------------------
+!
+! 1)  PART WHICH MUST BE DONE EVEN IF THERE IS NO OUTPUT FOR THIS STEP
+!     BUT ONLY AFTER FIRST TIME STEP FOR GRAPHIC PRINTOUTS
+!
+!-----------------------------------------------------------------------
+!
+      IF(NPERIAF.GT.0.AND.LT.EQ.0) THEN
+!       FOR OUTPUT OF INITIAL CONDITIONS
+        CALL OS('X=C     ',AMPL,AMPL,AMPL,0.D0)
+        CALL OS('X=C     ',PHAS,PHAS,PHAS,0.D0)
+      ENDIF
+!------------------------------------------------------------------
+!
+! 1)  PART WHICH MUST BE DONE EVEN IF THERE IS NO OUTPUT FOR THIS TIMESTEP
+!     BUT ONLY AFTER FIRST TIMESTEP FOR GRAPHIC PRINTOUTS
+!
+!-----------------------------------------------------------------------
+!
+      IF(LT.GE.PTINIG) THEN
+!
+!=======================================================================
+! COMPUTES THE MAXIMUM ELEVATION AND ASSOCIATED TIME
+!=======================================================================
+!
+      IF(SORLEO(27).OR.SORIMP(27)) THEN
+        IF(.NOT.DEJA1) THEN
+          CALL OS('X=Y     ',X=MAXZ ,Y=ZF)
+          CALL OS('X=C     ',X=TMAXZ,C=AT)
+          DEJA1=.TRUE.
+        ELSE
+          DO N=1,NPOIN
+            XMAX=H%R(N)+ZF%R(N)
+!           DRY LAND EXCLUDED (TO AVOID RANDOM TIMES)
+            IF(XMAX.GT.MAXZ%R(N).AND.H%R(N).GT.0.01D0) THEN
+              MAXZ%R(N)=XMAX
+              IF(SORLEO(28).OR.SORIMP(28)) TMAXZ%R(N)=AT
+            ENDIF
+          ENDDO
+        ENDIF
+      ENDIF
+!
+!=======================================================================
+! COMPUTES THE MAXIMUM SPEED AND ASSOCIATED TIME
+!=======================================================================
+!
+      IF(SORLEO(29).OR.SORIMP(29)) THEN
+        IF(.NOT.DEJA2) THEN
+          CALL OS('X=C     ',X=MAXV ,C=0.D0)
+          CALL OS('X=C     ',X=TMAXV,C=AT)
+          DEJA2=.TRUE.
+        ELSE
+          DO N=1,NPOIN
+            XMAX=SQRT(U%R(N)**2+V%R(N)**2)
+!           DRY LAND EXCLUDED (TO AVOID RANDOM TIMES)
+            IF(XMAX.GT.MAXV%R(N).AND.H%R(N).GT.0.01D0) THEN
+              MAXV%R(N)=XMAX
+              IF(SORLEO(30).OR.SORIMP(30)) TMAXV%R(N)=AT
+            ENDIF
+          ENDDO
+        ENDIF
+      ENDIF
+!
+!-----------------------------------------------------------------------
+!
+      ELSE
+!
+!     CASE WHERE OUTINI=.TRUE. : PRIORITY ON PTINIG, VALUES FOR LT=0
+!     OTHERWISE THEY WOULD NOT BE INITIALISED
+        IF(SORLEO(27).OR.SORIMP(27)) CALL OS('X=Y     ',X=MAXZ ,Y=ZF)
+        IF(SORLEO(28).OR.SORIMP(28)) CALL OS('X=C     ',X=TMAXZ,C=AT)
+        IF(SORLEO(29).OR.SORIMP(29)) CALL OS('X=C     ',X=MAXV ,C=0.D0)
+        IF(SORLEO(30).OR.SORIMP(30)) CALL OS('X=C     ',X=TMAXV,C=AT)
+!
+!     ENDIF FOR : IF(LT.GE.PTINIG) THEN
+      ENDIF
+!
+!
+!-----------------------------------------------------------------------
+!
+! 2)  PART WHICH MUST BE DONE ONLY IF THERE IS AN OUTPUT FOR THIS STEP
+!
+!-----------------------------------------------------------------------
 !
 !     PAS D'IMPRESSION, PAS DE SORTIE SUR FICHIER, ON RESSORT
       IF(.NOT.(LEO.OR.IMP)) GO TO 1000
