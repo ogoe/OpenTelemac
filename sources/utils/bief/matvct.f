a5f044eb0c3a5c0867e7ddbd975be092aacb776e
ct.pham <ct.pham@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-03-31 16:21:47 +0000

    [fix] Tentative correction to run hydraulic_jump TELEMAC-2D test case with quadratic elements (v2p0): STOX seems not to be initialised when this kind of elements. This may be improved to be cleaner.
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@7382 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: fe0ebbf83b1502af13a161ea6c0e48ed0e320c3e

--- sources/utils/bief/matvct.f
+++ sources/utils/bief/matvct.f
@@ -410,7 +410,7 @@
 !
       ELSEIF(IELM1.EQ.41.OR.IELM1.EQ.13) THEN
 !
-        IF(IELM2.EQ.41.OR.IELM2.EQ.13) THEN
+        IF(IELM2.EQ.41) THEN
 !
           IF(STOX.EQ.1) THEN
             CALL MV0606(OP, X , DA,TYPDIA,XA,TYPEXT, Y,C,
@@ -447,6 +447,14 @@
             CALL PLANTE(1)
             STOP
           ENDIF
+!
+        ELSEIF(IELM2.EQ.13) THEN
+!
+          CALL MV0606(OP, X , DA,TYPDIA,XA,TYPEXT, Y,C,
+     &                IKLE(1,1),IKLE(1,2),IKLE(1,3),
+     &                IKLE(1,4),IKLE(1,5),IKLE(1,6),
+     &                NPT,NELEM,NELMAX,
+     &                W(1,1),W(1,2),W(1,3),W(1,4),W(1,5),W(1,6))
 !
         ELSEIF(IELM2.EQ.11) THEN
 !
