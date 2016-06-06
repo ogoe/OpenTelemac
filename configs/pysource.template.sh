03e18de90cc0a48556f2175cb93a6cb7199ee3f0
y.audouin <y.audouin@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-06-06 13:04:55 +0000

    [config] Adding template for environement positioning files
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@7636 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb
    
    Former-commit-id: 8a846b9dd8c02c76b609c72c6e15bece92a9920a

---
+++ configs/pysource.template.sh
@@ -0,0 +1,41 @@
+# This file is a template for a Linux environement file 
+# runnning "source pysource.template.sh" will position all 
+# the necessary environement variable for telemac
+# To adapt to your installation replace word <word> by their local value
+###
+### TELEMAC settings -----------------------------------------------------------
+###
+# Path to telemac root dir
+export HOMETEL=<path_to_telemac_root_dir>
+# Adding python scripts to PATH
+export PATH=$HOMETEL/scripts/python27:.:$PATH
+# Path to this file
+export SOURCEFILE=$HOMETEL/configs/pysource.template.sh
+# Configuration file
+export SYSTELCFG=$HOMETEL/configs/systel.cfg
+# Name of the configuration to use
+export USETELCFG=<myconfiguration>
+### Python
+# To force python to flush its output
+export PYTHONUNBUFFERED='true'
+###
+### COMPILERS -----------------------------------------------------------
+###
+
+# Here are a few exemple for external libraries
+
+### MPI -----------------------------------------------------------
+#export MPIHOME=$SYSTEL/LIBRARY/mpi/ifort.10.1.008
+#export PATH=$MPIHOME/bin:$PATH
+#export LD_LIBRARY_PATH=$MPIHOME/lib:$LD_LIBRARY_PATH
+###
+### EXTERNAL LIBRARIES -----------------------------------------------------------
+###
+### HDF5 -----------------------------------------------------------
+#export HDF5HOME=$SYSTEL/LIBRARY/hdf5-1.8.11/arch/C9
+#export LD_LIBRARY_PATH=$HDF5HOME/lib:$LD_LIBRARY_PATH
+#export LD_RUN_PATH=$HDF5HOME/lib:$MEDHOME/lib:$LD_RUN_PATH
+### MED  -----------------------------------------------------------
+#export MEDHOME=$SYSTEL/LIBRARY/med-3.0.6/arch/C9
+#export LD_LIBRARY_PATH=$MEDHOME/lib:$LD_LIBRARY_PATH
+#export PATH=$MEDHOME/bin:$PATH
