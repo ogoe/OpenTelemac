7f3ca4d721066c78774611587c59354f51e6aa61
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-07-16 16:53:56 +0000

    [fix] positive_depths and cvtrvf_pos: now forbidding segments with opposite fluxes in adjacent triangles,  so that the flux limitation after assembly is in the range [0,1].  This slightly changes the new ERIA advection scheme (for the better), and enables TREATMENT OF NEGATIVE DEPTHS = 3 for the LIPS scheme. Control in lecdon_telemac2d subsequently removed. ERIA and LIPS can now be used in the same run.
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@7883 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb

--- sources/telemac2d/lecdon_telemac2d.f
+++ sources/telemac2d/lecdon_telemac2d.f
@@ -100,13 +100,6 @@
 !+        V7P2
 !+  Allocating NAMETRAC added.
 !
-!history  J-M HERVOUET (EDF LAB, LNHE)
-!+        14/07/2016
-!+        V7P2
-!+  Important verifications added: scheme LIPS must have
-!+  TREATMENT OF NEGATIVE DEPTHS = 2 (it is thus incompatible with
-!+  scheme ERIA=15.
-!
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !| FILE_DESC      |<--| STORES STRINGS 'SUBMIT' OF DICTIONARY
 !| MOTCAR         |<--| VALUES OF KEY-WORDS OF TYPE CHARACTER
@@ -2507,76 +2500,6 @@
                 OPTCHA=2
               ENDIF
             ENDDO
-          ENDIF
-        ENDIF
-      ENDIF
-!
-!-----------------------------------------------------------------------
-!
-!     LIPS SCHEME IS INCOMPATIBLE WITH TREATMENT OF NEGATIVE DEPTHS = 3
-!
-!     IT IS CHECKED FOR TRACERS, VELOCITIES, AND K-EPSILON
-!
-      IF(NTRAC.GT.0.AND.EQUA(14:15).EQ.'EF') THEN
-        DO ITRAC=1,NTRAC
-          IF(    (ICONVFT(ITRAC).EQ.ADV_PSI
-     &        .OR.ICONVFT(ITRAC).EQ.ADV_NSC).AND.
-     &        OPTADV_TR(ITRAC).EQ.4) THEN
-            IF(.NOT.BANDEC.OR.OPTBAN.NE.1.OR.OPT_HNEG.NE.2) THEN
-              IF(LNG.EQ.1) THEN
-                WRITE(LU,*) 'AVEC LE SCHEMA DE CONVECTION'
-                WRITE(LU,*) 'DES TRACEURS ',ADV_NSC,' OU ',ADV_PSI,
-     &                      ' ET OPTION 4 (LIPS)'
-                WRITE(LU,*) 'TRAITEMENT DES HAUTEURS NEGATIVES'
-                WRITE(LU,*) 'DOIT ETRE EGAL A 2'
-                WRITE(LU,*) 'BANCS DECOUVRANTS DOIT ETRE EGAL A OUI'
-                WRITE(LU,*) 'OPTION DE TRAITEMENT DES BANCS DECOUVRANTS'
-                WRITE(LU,*) 'DOIT ETRE EGAL A 1'
-              ENDIF
-              IF(LNG.EQ.2) THEN
-                WRITE(LU,*) 'WITH ADVECTION SCHEME FOR TRACERS'
-                WRITE(LU,*) ADV_NSC,' OR ',ADV_PSI,
-     &                      ' AND OPTION 4 (LIPS)'
-                WRITE(LU,*) 'TREATMENT OF NEGATIVE DEPTHS'
-                WRITE(LU,*) 'MUST BE EQUAL TO 2'
-                WRITE(LU,*) 'TIDAL FLATS MUST BE EQUAL TO YES'
-                WRITE(LU,*) 'OPTION FOR THE TREATMENT OF TIDAL'
-                WRITE(LU,*) 'FLATS MUST BE EQUAL TO 1'
-              ENDIF
-              CALL PLANTE(1)
-              STOP
-            ENDIF
-          ENDIF
-        ENDDO
-      ENDIF
-      IF(EQUA(14:15).EQ.'EF') THEN
-        IF(  ((ICONVF(1).EQ.ADV_PSI.OR.
-     &         ICONVF(1).EQ.ADV_NSC).AND.OPTADV_VI.EQ.4)
-     &   .OR.((ICONVF(4).EQ.ADV_PSI.OR.
-     &         ICONVF(4).EQ.ADV_NSC).AND.OPTADV_KE.EQ.4) ) THEN
-          IF(.NOT.BANDEC.OR.OPTBAN.NE.1.OR.OPT_HNEG.NE.2) THEN
-            IF(LNG.EQ.1) THEN
-              WRITE(LU,*) 'AVEC LES SCHEMAS POUR LA CONVECTION'
-              WRITE(LU,*) 'DES VITESSES OU DU K-EPSILON'
-              WRITE(LU,*) ADV_NSC,' OU ',ADV_PSI,' ET OPTION 4 (LIPS)'
-              WRITE(LU,*) 'TRAITEMENT DES HAUTEURS NEGATIVES'
-              WRITE(LU,*) 'DOIT ETRE EGAL A 2'
-              WRITE(LU,*) 'BANCS DECOUVRANTS DOIT ETRE EGAL A OUI'
-              WRITE(LU,*) 'OPTION DE TRAITEMENT DES BANCS DECOUVRANTS'
-              WRITE(LU,*) 'DOIT ETRE EGAL A 1'
-            ENDIF
-            IF(LNG.EQ.2) THEN
-              WRITE(LU,*) 'WITH SCHEME FOR ADVECTION'
-              WRITE(LU,*) 'OF VELOCITIES OR K-EPSILON'
-              WRITE(LU,*) ADV_NSC,' OR ',ADV_PSI,' AND OPTION 4 (LIPS)'
-              WRITE(LU,*) 'TREATMENT OF NEGATIVE DEPTHS'
-              WRITE(LU,*) 'MUST BE EQUAL TO 2'
-              WRITE(LU,*) 'TIDAL FLATS MUST BE EQUAL TO YES'
-              WRITE(LU,*) 'OPTION FOR THE TREATMENT OF TIDAL'
-              WRITE(LU,*) 'FLATS MUST BE EQUAL TO 1'
-            ENDIF
-            CALL PLANTE(1)
-            STOP
           ENDIF
         ENDIF
       ENDIF
