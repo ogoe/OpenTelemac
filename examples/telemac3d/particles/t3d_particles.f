ac871c1d61f161989eb975570c32935ea39cba0c
ct.pham <ct.pham@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-09-14 15:45:33 +0000

    [VnV] [telemac3d] Update of nomvar_2d_in_3d subroutine
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@8357 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb

--- examples/telemac3d/particles/t3d_particles.f
+++ examples/telemac3d/particles/t3d_particles.f
@@ -48,9 +48,9 @@
 !| TEXTPR         |<->| SEE ABOVE
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !
-      USE DECLARATIONS_SPECIAL
       USE DECLARATIONS_TELEMAC3D, ONLY: NCOUCH, NLAYMAX, MIXTE
 !
+      USE DECLARATIONS_SPECIAL
       IMPLICIT NONE
 !
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -118,6 +118,7 @@
       TEXTE (35) = 'HIGH WATER MARK M               '
       TEXTE (36) = 'HIGH WATER TIME S               '
       TEXTE (37) = 'BED EVOLUTION   M               '
+      TEXTE (38) = 'AIR TEMPERATURE DEGREE          '
 !
 ! TEXTPR IS USED TO READ PREVIOUS COMPUTATION FILES.
 ! IN GENERAL TEXTPR=TEXTE BUT YOU CAN FOLLOW UP A COMPUTATION
@@ -170,6 +171,7 @@
       TEXTPR (35) = 'HIGH WATER MARK M               '
       TEXTPR (36) = 'HIGH WATER TIME S               '
       TEXTPR (37) = 'BED EVOLUTION   M               '
+      TEXTPR (38) = 'AIR TEMPERATURE DEGREE          '
 !
 !-----------------------------------------------------------------------
 !
@@ -214,6 +216,7 @@
       TEXTE (35) = 'COTE MAXIMUM    M               '
       TEXTE (36) = 'TEMPS COTE MAXI S               '
       TEXTE (37) = 'EVOLUTION FOND  M               '
+      TEXTE (38) = 'TEMPERATURE AIR DEGREE          '
 !
 ! TEXTPR SERT A LA LECTURE DES FICHIERS DE CALCULS PRECEDENTS
 ! A PRIORI TEXTPR=TEXTE MAIS ON PEUT ESSAYER DE FAIRE UNE SUITE
@@ -256,6 +259,7 @@
       TEXTPR (35) = 'COTE MAXIMUM    M               '
       TEXTPR (36) = 'TEMPS COTE MAXI S               '
       TEXTPR (37) = 'EVOLUTION FOND  M               '
+      TEXTPR (38) = 'TEMPERATURE AIR DEGREE          '
 !
 !-----------------------------------------------------------------------
 !
@@ -349,12 +353,14 @@
       MNEMO(36)   = 'TMXZ    '
 !     BED EVOLUTION
       MNEMO(37)   = 'DZF     '
+!     AIR TEMPERATURE
+      MNEMO(38)   = 'TAIR    '
 !
 !-----------------------------------------------------------------------
 !
 !     TRACERS
 !
-      NEXT = 37+1
+      NEXT = 38+1
 !
       IF(NTRAC.GT.0) THEN
         DO I=1,NTRAC
@@ -419,7 +425,7 @@
         ENDIF
         MNEMO(NEXT+1) = 'PVSNCO  '
       ENDIF
-
+!
       DO I=38,100
         TEXTPR(I)=TEXTE(I)
       ENDDO
