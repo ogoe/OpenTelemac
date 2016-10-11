9708cec2c9a88704ad49f4a7fcd337cf182c8a56
r.kopmann <r.kopmann@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-10-11 10:53:02 +0000

    add new multigrain / multilayer example
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@8544 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb

---
+++ examples/sisyphe/yen_multigrain/t2d_yen-exp.f
@@ -0,0 +1,176 @@
+!                       *****************
+                        SUBROUTINE CORFON
+!                       *****************
+!
+!***********************************************************************
+! TELEMAC 2D VERSION 5.2          01/03/90    J-M HERVOUET
+!***********************************************************************
+!
+!  USER SUBROUTINE CORFON
+!
+!  FUNCTION  : MODIFICATION OF THE BOTTOM TOPOGRAPHY
+!
+!
+!-----------------------------------------------------------------------
+!  ARGUMENTS USED IN THE EXAMPLE
+! .________________.____.______________________________________________
+! |      NOM       |MODE|                   ROLE
+! |________________|____|_______________________________________________
+! |      ZF        |<-->| FOND A MODIFIER.
+! |      X,Y,(Z)   | -->| COORDONNEES DU MAILLAGE (Z N'EST PAS EMPLOYE).
+! |      A         |<-- | MATRICE
+! |      T1,2      | -->| TABLEAUX DE TRAVAIL (DIMENSION NPOIN)
+! |      W1        | -->| TABLEAU DE TRAVAIL (DIMENSION 3 * NELEM)
+! |      NPOIN     | -->| NOMBRE DE POINTS DU MAILLAGE.
+! |      PRIVE     | -->| TABLEAU PRIVE POUR L'UTILISATEUR.
+! |      LISFON    | -->| NOMBRE DE LISSAGES DU FOND.
+! |________________|____|______________________________________________
+! MODE : -->(DONNEE NON MODIFIEE), <--(RESULTAT), <-->(DONNEE MODIFIEE)
+!-----------------------------------------------------------------------
+!
+! PROGRAMME APPELANT :
+! PROGRAMMES APPELES : RIEN EN STANDARD
+!
+!***********************************************************************
+!
+      USE BIEF
+      USE DECLARATIONS_TELEMAC2D
+!
+      USE DECLARATIONS_SPECIAL
+      IMPLICIT NONE
+      DOUBLE PRECISION L, MPZ
+      INTEGER I
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      LOGICAL MAS
+!
+!-----------------------------------------------------------------------
+!
+!  LISSAGES EVENTUELS DU FOND
+!
+      IF(LISFON.GT.0) THEN
+!
+        MAS=.TRUE.
+        CALL FILTER(ZF,MAS,T1,T2,AM1,'MATMAS          ',
+     &              1.D0,T1,T1,T1,T1,T1,T1,MESH,MSK,MASKEL,LISFON)
+!
+      ENDIF
+!
+!-----------------------------------------------------------------------
+!
+      MPZ = 9.964433629385642D0
+!
+      DO I=1,NPOIN
+!
+! part1: straight flume left
+! at the end (11.5 m, z = 9.977)
+        IF (X(I).LE. 101.1819D0.AND.y(i).LE.42.6371D0) THEN
+          ZF%R(I) = 10.D0 - ABS(Y(I)-31.1371D0)*0.002D0
+        ENDIF
+!    
+! part II: bend 
+!
+! midpoint is at x 101.1819 and 104.6819 + 4.0
+! the length of the circle part is l_circle_part = (pi*4.0)/2.0
+! l_circle_part = 6.283185307179586 (1/4 of the circle)
+! z at the start of the circle part = 9.977
+! z at the middle of the circle part = 9.964433629385642
+! z at the end of the circle part = 9.951867258771284
+! radius is 4.0
+!
+        IF (Y(I).GT.42.6371D0) THEN
+           L=3.141592654D0*4.0D0*                                                                                        
+     &     (1.5707963D0-DACOS((ABS(X(I)-101.1819D0))/                                                                     
+     &     (SQRT((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2))))/                                                            
+     &     3.141592654D0 
+!
+          IF(X(I).LT.101.1819D0) THEN
+              ZF%R(I) = L*(0.002D0) + MPZ
+          ENDIF
+!
+          IF(X(I).GE.101.1819D0) THEN
+            ZF%R(I) = L*(-0.002D0) + MPZ 
+          ENDIF
+        ENDIF
+!
+! part III: straight part right
+! starts with ~ 9.961192852465999
+        IF (x(I).GE.101.1819D0.AND.Y(I).LE.42.6371D0) THEN
+          ZF%R(I)= 9.951867258771284D0-(42.6371D0-Y(I))*(0.002D0) 
+        ENDIF 
+!    
+      END DO
+
+
+! Change of  Bottom slope
+!      DO I=1,NPOIN
+!        IF (X(I).LE.101.1819D0.AND.Y(I).LE.42.6371) THEN
+!
+!        ENDIF
+!        IF (Y(I).GT.42.6371D0) THEN
+!!         IF (((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2).GE.12.0409D0
+!!     *  .AND.((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2)
+!!     *  .LE.20.5209D0) THEN
+! Calculating the length of a circle section
+!          IF (X(I).LT.101.1819D0) THEN
+!!          R=SQRT((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2)
+!            L=3.141592654D0*4.0D0*
+!     &     (1.5707963D0-DACOS((ABS(X(I)-101.1819D0))/
+!     &     (SQRT((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2))))/
+!     &     3.141592654D0
+!            ZF%R(I) = L*(0.0016D0) + 9.97072D0
+!          ENDIF
+!          IF (X(I).GT.101.1819D0) THEN
+!!          R=SQRT((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2)
+!            L=3.141592654D0*4.D0*
+!     &     (1.5707963D0-DACOS((ABS(X(I)-101.1819D0))/
+!     &     (SQRT((X(I)-101.1819D0)**2+(Y(I)-42.6371D0)**2))))/
+!     &     3.141592654D0
+!            ZF%R(I) = L*(-0.0016D0) + 9.97072D0
+!          ENDIF
+!!         ENDIF
+!        ENDIF
+!        IF (X(I).GE.101.1819D0.AND.Y(I).LE.42.6371D0) THEN
+!          ZF%R(I) = (ABS(Y(I)-42.6371D0))*(-0.0016D0) + 9.95954D0
+!        ENDIF
+!        IF (X(I).LE.101.1819D0.AND.Y(I).LE.42.8D0.AND.Y(I).GE.42.5D0)
+!     &  THEN
+!          ZF%R(I) = 9.98145D0
+!        ENDIF
+!        IF (X(I).GE.101.1819D0.AND.Y(I).LE.42.6D0.AND.Y(I).GE.42.45D0)
+!     &  THEN
+!          ZF%R(I) = 9.960055D0
+!        ENDIF
+!      END DO
+!
+      IF(LNG.EQ.1) THEN
+        IF(LISFON.EQ.0) THEN
+          WRITE(LU,*)
+          WRITE(LU,*) 'CORFON (TELEMAC2D) : PAS DE MODIFICATION DU FOND'
+          WRITE(LU,*)
+        ELSE
+          WRITE(LU,*)
+          WRITE(LU,*) 'CORFON (TELEMAC2D) : ',LISFON,' LISSAGES DU FOND'
+          WRITE(LU,*)
+        ENDIF
+      ENDIF
+      IF(LNG.EQ.2) THEN
+        IF(LISFON.EQ.0) THEN
+          WRITE(LU,*)
+          WRITE(LU,*) 'CORFON (TELEMAC2D): NO MODIFICATION OF BOTTOM'
+          WRITE(LU,*)
+        ELSE
+          WRITE(LU,*)
+          WRITE(LU,*) 'CORFON (TELEMAC2D): ',LISFON,' BOTTOM SMOOTHINGS'
+          WRITE(LU,*)
+        ENDIF
+      ENDIF
+!
+!-----------------------------------------------------------------------
+!
+      RETURN
+      END
---
+++ examples/sisyphe/yen_multigrain/yen.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+"""Provides a function to plot the validation example for YEN from Telemac csv-files
+
+We use the python built in csv-reader. There is no CSV standard we asume that there 
+is a Telemac standard
+
+Please notice: 
+    - The evolution must be devided by 0.0544
+    - The following lines are needed on a server without graphical screen  
+        import matplotlib as mpl
+        mpl.use('Agg')
+
+"""
+import matplotlib as mpl
+mpl.use('Agg')
+from os import getcwd,path,sep
+import sys
+import csv
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.tri as tri
+
+################################################################################
+#####    Will allow to use pytel scripts from the test case folder    ##########
+################################################################################
+actualPath =  getcwd()
+pathsplit = actualPath.split(sep)
+pytelpath = ""
+pytelpath += sep
+
+for i in range(len(pathsplit)):
+    if "example" in pathsplit[i]:
+        break
+    if len(pathsplit[i]) > 0:
+        pytelpath = path.join(pytelpath,pathsplit[i])
+ 
+pytelpath = path.join(pytelpath,'scripts','python27')
+sys.path.append(pytelpath)
+
+#hardcoded workarround for paths
+sys.path.append("/panfs/sw/apps/telemac/v6p3_wels/wels/scripts/python27")
+
+################################################################################
+#####    Dependencies towards other pytel/modules                     ##########
+################################################################################
+
+import parsers.parserSELAFIN as parserSlf
+
+__authors__="Rebekka Kopmann, Leopold Stadler"
+__maintainer__ = "Rebekka Kopmann"
+__version__ = "1.0"
+
+
+
+def getPositionOfVariable(varnames,searchName):
+
+   for i,name in enumerate(varnames):
+      if name.strip() == searchName:
+         return i
+
+   print "Exit Variable %s not found in varnames" %(searchName)
+   sys.exit()
+
+   return
+
+if __name__ == "__main__":
+
+   print "debug",getcwd()
+
+   if len(sys.argv) < 2:
+      sys.exit("No arguments please give an input file and outputname")
+
+   #########################################################################
+   #
+   # factor is used for scaling (evolution/h0_factor = evolutionCut)
+   # See the paper of Yen et al. 1995
+   #########################################################################
+    
+   selafinfile = sys.argv[1]
+   slf = parserSlf.SELAFIN(selafinfile) 
+   mesh = np.array(slf.IKLE3)
+   triang = tri.Triangulation(slf.MESHX,slf.MESHY,mesh)
+   timePos = len(slf.tags["times"]) -1
+
+   values = slf.getVALUES(timePos)
+   varPos = getPositionOfVariable(slf.VARNAMES,'EVOLUTION')
+   evolution = values[varPos]
+   evolution_interpolator = tri.LinearTriInterpolator(triang,evolution)
+
+   #########################################################################
+   #
+   # Read the reference file
+   #
+   #########################################################################
+   h0_factor = 0.0544
+    
+   #load profile 90 
+   data_profile_90 =  np.genfromtxt("yen_90profilexy-koor.dat",names=["x","y","z"])
+   profileRef90X = data_profile_90["x"]
+   profileRef90Y = data_profile_90["y"]
+   profileRef90Z = data_profile_90["z"]
+   evolutionCut_90 = evolution_interpolator.__call__(profileRef90X,profileRef90Y)/h0_factor
+
+   distance_profile_90 = profileRef90Y - 46.1371
+
+   #load profile 180 
+   data_profile_180 =  np.genfromtxt("yen_180profilexy-koor.dat",names=["x","y","z"])
+   profileRef180X = data_profile_180["x"]
+   profileRef180Y = data_profile_180["y"]
+   profileRef180Z = data_profile_180["z"]
+   evolutionCut_180 = evolution_interpolator.__call__(profileRef180X,profileRef180Y)/h0_factor
+
+   distance_profile_180 = profileRef180X - 104.682
+
+
+   #########################################################################
+   #
+   # Plot with matplotlib
+   #
+   #########################################################################
+
+   #plot 90
+   plt.plot(distance_profile_90, profileRef90Z, "o-",color="darkorange", label = "Reference")
+   plt.plot(distance_profile_90, evolutionCut_90,"<--",color="green", label = "Simulation")
+   plt.xlabel("Distance [m]")
+   plt.ylabel("Evolution [m]")
+   plt.grid()
+   plt.xlim([0.0,1.0])
+   plt.legend()
+   title = sys.argv[2] +"_90"
+   plt.title(title)
+   plt.savefig(title + ".pdf")
+   plt.savefig(title + ".png")
+   plt.clf()
+
+   #plot 180
+   plt.plot(distance_profile_180, profileRef180Z, "o-",color="darkorange", label = "Reference")
+   plt.plot(distance_profile_180, evolutionCut_180,"<--",color="green", label = "Simulation")
+   plt.xlabel("Distance [m]")
+   plt.ylabel("Evolution [m]")
+   plt.grid()
+   plt.xlim([0.0,1.0])
+   plt.legend()
+   title = sys.argv[2] + "_180"
+   plt.title(title)
+   plt.savefig(title + ".pdf")
+   plt.savefig(title + ".png")
+   plt.clf()
+
+
