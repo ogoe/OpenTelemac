fa8a99f18cdac64027ab8d367f1a84e9a1765771
jm.hervouet <jm.hervouet@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2016-11-23 20:16:43 +0000

    [fix] bief: correcting misprints and French English swapping in CHECK_CALL and WRITE_ERROR.
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@8775 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb

--- sources/utils/special/check_call.f
+++ sources/utils/special/check_call.f
@@ -34,15 +34,16 @@
 !
       IF(IERR.NE.0) THEN
         IF(LNG.EQ.1) THEN
-          WRITE(LU,*) 'ERROR ',IERR,'DURING CALL OF ',CHFILE
-          WRITE(LU,*) 'ERROR TEXTE: ',WRITE_ERROR(IERR)
+          WRITE(LU,*) 'ERREUR ',IERR,'LORS DE L''APPEL A : ',
+     &                          CHFILE
+          WRITE(LU,*) 'TEXTE DE L''ERREUR : ',WRITE_ERROR(IERR)
         ENDIF
         IF(LNG.EQ.2) THEN
-          WRITE(LU,*) 'ERREUR ',IERR,'LORS DE L APPEL A ',
-     &                          CHFILE
-          WRITE(LU,*) 'TEXTE DE L''ERROR : ',WRITE_ERROR(IERR)
+          WRITE(LU,*) 'ERROR ',IERR,'DURING CALL OF ',CHFILE
+          WRITE(LU,*) 'ERROR TEXT: ',WRITE_ERROR(IERR)
         ENDIF
         CALL PLANTE(1)
+        STOP
       ENDIF
 !
 !-----------------------------------------------------------------------
