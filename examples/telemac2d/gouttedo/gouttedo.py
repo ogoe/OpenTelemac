945557a4f00280d7f7fce259ba8e3249e42f0efe
y.audouin <y.audouin@21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb>
2017-01-17 07:09:27 +0000

    [VnV] Update of python api script of gouttedo
    git-svn-id: http://svn.opentelemac.org/svn/opentelemac/trunk@9043 21fd64ea-cbdc-4a4a-8fb2-7ac88764eefb

--- examples/telemac2d/gouttedo/gouttedo.py
+++ examples/telemac2d/gouttedo/gouttedo.py
@@ -6,9 +6,11 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 t2d = Telemac2d('t2d_gouttedo.cas',user_fortran ='t2d_gouttedo.f',comm=comm)
 ierr = 0
+rank = comm.Get_rank()
+ncsize = comm.Get_size()
 # Running partel
-if comm.Get_rank == 0:
-    ierr = t2d.api_inter.run_partel(t2d.id,'geo_gouttedo.slf','geo_gouttedo.cli',4,1,'SERAFIN ',' ',' ',' ')
+if ( rank == 0 and ncsize > 1):
+    ierr = t2d.api_inter.run_partel(t2d.id,'geo_gouttedo.slf','geo_gouttedo.cli',ncsize,1,'SERAFIN ',' ',' ',' ')
 comm.Barrier()
 print ierr
 t2d.set_case()
@@ -18,9 +20,9 @@ t2d.init_state_default()
 t2d.run_all_time_steps()
 # Running gretel
 comm.Barrier()
-if comm.Get_rank == 0:
-    t2d.api_inter.run_gretel(t2d.id,'geo_gouttedo.slf','SERAFIN ','r2d_gouttedo_v1p0.slf','SERAFIN ',4,0)
 # Ending the run
 t2d.finalize()
+if ( rank == 0 and ncsize > 1):
+    t2d.api_inter.run_gretel(t2d.id,'geo_gouttedo.slf','SERAFIN ','r2d_gouttedo_v1p0.slf','SERAFIN ',ncsize,0)
 # Instance delete
 del(t2d)
